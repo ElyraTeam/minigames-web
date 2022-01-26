93907429428f21eec7c19c74e51896f0e9b60ea6
devkarim <devkarim@hotmail.com>
2022-01-26 03:30:26 +0200

    create word page improvements

--- pages/games/word/create.tsx
+++ pages/games/word/create.tsx
@@ -1,64 +1,222 @@
-import { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
+import { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
 
-import { FaArrowRight, FaTimes, FaPlusSquare } from "react-icons/fa";
+import {
+  FaArrowLeft,
+  FaArrowRight,
+  FaTimes,
+  FaPlusSquare,
+  FaRedoAlt,
+} from 'react-icons/fa';
+
+const DEFAULT_CATEGORIES_ARABIC = [
+  'ولد',
+  'بنت',
+  'حيوان',
+  'جماد',
+  'نبات',
+  'بلد',
+  'فيلم',
+  'حشرة',
+  'لون',
+  'مدينة',
+];
+
+const charsArabic: string[] = [
+  'أ',
+  'ب',
+  'ت',
+  'ث',
+  'ج',
+  'ح',
+  'خ',
+  'د',
+  'ذ',
+  'ر',
+  'ز',
+  'س',
+  'ش',
+  'ص',
+  'ض',
+  'ط',
+  'ظ',
+  'ع',
+  'غ',
+  'ف',
+  'ق',
+  'ك',
+  'ل',
+  'م',
+  'ن',
+  'هـ',
+  'و',
+  'ى',
+];
 
 const WordCreate: NextPage = () => {
-    const charsArabic: string[] = ["أ", "ب", "ت", "ث", "ج", "ح", "خ", "د", "ذ", "ر", "ز", "س", "ش", "ص", "ض", "ط", "ظ", "ع", "غ", "ف", "ق", "ك", "ل", "م", "ن", "هـ", "و", "ى"]
-    const categoriesArabic: string[] = ["اسم ولد", "اسم بنت", "حيوان", "جماد", "نبات", "بلد", "فيلم", "حشرة", "لون", "مدينة"]
+  const [categoriesArabic, setCategories] = useState(DEFAULT_CATEGORIES_ARABIC);
+  const [charsSelected, setCharsSelected] = useState<string[]>(
+    charsArabic.slice(0, 8)
+  );
+  const [newCategory, setNewCategory] = useState('');
+  const [newCategoryError, setNewCategoryError] = useState<string | null>(null);
 
-    const clickedChars: string[] = [];
-
-
-    function charClick(e: any) {
-        if (e.target.classList.contains("active")) {
-            e.target.classList.remove("active");
-            clickedChars.splice(clickedChars.indexOf(e.target.textContent), 1);
-        } else {
-            e.target.classList.add("active");
-            clickedChars.push(e.target.textContent);
-        }
+  function charClick(char: string) {
+    if (charsSelected.includes(char)) {
+      setCharsSelected(charsSelected.filter((c) => c != char));
+    } else {
+      setCharsSelected([...charsSelected, char]);
     }
+  }
 
-    return (
-        <div className="word-create-main h-screen bg-[url('../public/wordbackground.svg')] bg-cover flex justify-center items-center">
-            <div className="main-content-box bg-light sm:px-8 pb-5 pt-3 rounded-2xl text-center border-4 border-white shadow-[0_16px_32px_0_rgba(0,0,0,0.4)]">
-                <Head>
-                    <title>Word - Create</title>
-                </Head>
+  function deleteCategory(categoryName: string) {
+    if (categoriesArabic.length == 3) return;
+    setCategories(categoriesArabic.filter((cat) => cat != categoryName));
+  }
 
-                <Image src="/wordlogo.svg" width="85" height="85" alt="logo" />
+  function resetCategories() {
+    setCategories(DEFAULT_CATEGORIES_ARABIC);
+  }
 
-                <div className="content-box bg-dark lg:px-8 md:px-32 py-8 rounded-2xl mb-5 mt-3 mx-5 scrollbar overflow-y-scroll max-h-96">
-                    <div className="choose-chars">
-                        <h2 className="mb-5 text-white text-2xl font-semibold">اختر الحروف</h2>
-                        <div className="chars grid grid-cols-7">
-                            {charsArabic.map(char => {
-                                return <div key={charsArabic.indexOf(char)} onClick={charClick} className="py-2 px-3 text-lg m-2 bg-white cursor-pointer rounded-full font-semibold flex justify-center items-center shadow-[0_4px_8px_0_rgba(0,0,0,0.3)] transition-colors hover:bg-primary hover:text-white">{char}</div>
-                            })}
-                        </div>
+  function addCategory(categoryName: string) {
+    if (categoryName.length == 0) return;
+    if (categoriesArabic.includes(categoryName))
+      return setNewCategoryError('لا يمكنك اضافة نفس الكلمة مرتين');
+    setCategories([...categoriesArabic, categoryName]);
+    setNewCategory('');
+    setNewCategoryError(null);
+  }
 
-                        <div className="choose-categories mt-32">
-                            <h2 className="mb-5 text-white text-2xl font-semibold">اختر التصنيفات</h2>
-                            <div className="categories grid grid-cols-6">
-                                {categoriesArabic.map(category => {
-                                    return <div key={categoriesArabic.indexOf(category)} className="py-3 px-8 mx-2 my-2 text-lg bg-white rounded-3xl flex justify-center items-center shadow-[0_4px_8px_0_rgba(0,0,0,0.3)]">{category}</div>
-                                })}
-                            </div>
-                            <div className="add-category mt-8 relative inline-block text-white">
-                                <input type="text" placeholder="أضف تصنيفا" className="bg-transparent border-0 border-b-2 p-2 text-2xl text-right text-white placeholder:text-white focus:outline-none" />
-                                <FaPlusSquare className="absolute top-4 left-2 rounded-full text-2xl cursor-pointer" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="word-create-main h-screen bg-[url('../public/wordbackground.svg')] bg-cover flex justify-center items-center">
+      <div className="main-content-box bg-light sm:px-8 pb-5 pt-3 rounded-2xl text-center border-4 border-white shadow-[0_16px_32px_0_rgba(0,0,0,0.4)] max-w-4xl ">
+        <Head>
+          <title>Word - Create</title>
+        </Head>
 
-                <Link href="/"><h3 className="text-white mr-10 text-xl cursor-pointer float-right hover:text-black font-semibold">الرئيسية <FaArrowRight className="inline ml-2" /></h3></Link>
+        <Image src="/wordlogo.svg" width="85" height="85" alt="logo" />
+
+        <div className="content-box bg-dark lg:px-8 md:px-32 py-8 rounded-2xl mb-5 mt-3 mx-5 scrollbar overflow-y-scroll max-h-96">
+          <div className="choose-chars overflow-hidden">
+            <h2 className="mb-5 text-white text-2xl font-semibold">الحروف</h2>
+            <div
+              className="chars grid lg:grid-cols-12 md:grid-cols-10 grid-cols-6"
+              dir="rtl"
+            >
+              {charsArabic.map((char, i) => {
+                return (
+                  <div
+                    key={charsArabic.indexOf(char)}
+                    onClick={() => charClick(char)}
+                    className={
+                      'py-2 px-3 text-lg m-2 bg-white cursor-pointer rounded-full font-semibold flex justify-center items-center shadow-[0_4px_8px_0_rgba(0,0,0,0.3)] transition-colors hover:bg-primary hover:text-white w-10 ' +
+                      (charsSelected.includes(char) ? 'active' : '')
+                    }
+                  >
+                    {char}
+                  </div>
+                );
+              })}
             </div>
+
+            <div className="choose-categories mt-16">
+              <h2 className="mb-5 text-white text-2xl font-semibold">
+                الفئات
+                <FaRedoAlt
+                  className="inline ml-2 bg-[#92E4AB] p-2 rounded-full cursor-pointer text-3xl"
+                  onClick={resetCategories}
+                />
+              </h2>
+              <div className="categories flex flex-wrap" dir="rtl">
+                {categoriesArabic.map((category) => {
+                  return (
+                    <div
+                      key={categoriesArabic.indexOf(category)}
+                      className="relative py-3 px-8 mx-2 my-2 text-lg bg-white rounded-3xl flex justify-center items-center shadow-[0_4px_8px_0_rgba(0,0,0,0.3)] w-50"
+                    >
+                      <FaTimes
+                        className="absolute -top-1 -right-1 bg-[#f00] text-white text-[1.6rem] rounded-3xl p-1 cursor-pointer"
+                        onClick={() => deleteCategory(category)}
+                      ></FaTimes>
+                      {category}
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="add-category mt-8 relative inline-block text-white">
+                <input
+                  type="text"
+                  placeholder="أضف فئة"
+                  className="bg-transparent border-0 border-b-2 border-b-[#eee] p-2 text-right text-white placeholder:text-white focus:outline-none text-lg focus:border-b-light transition-[border]"
+                  maxLength={15}
+                  value={newCategory}
+                  onChange={(input) => setNewCategory(input.target.value)}
+                />
+                <FaPlusSquare
+                  className="absolute top-2 left-2 rounded-full text-2xl cursor-pointer"
+                  onClick={() => addCategory(newCategory)}
+                />
+              </div>
+              {newCategoryError && (
+                <p className="text-[#f00] mt-4 font-semibold">
+                  {newCategoryError}
+                </p>
+              )}
+            </div>
+          </div>
+          <div className="choose-categories mt-16">
+            <h2 className="mb-8 text-white text-2xl font-semibold">
+              الاعدادات
+            </h2>
+            <div className="flex justify-center align-middle text-white text-center font-light">
+              <div className="mr-12">
+                <span className="text-lg">عدد الجولات</span>
+                <br />
+                <select className="bg-transparent border-b-2 my-1 w-12 focus:outline-none">
+                  {Array.from(
+                    { length: charsSelected.length },
+                    (x, i) => i + 1
+                  ).map((num) => (
+                    <option key={num} value={num}>
+                      {num}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div>
+                <span className="text-lg">عدد اللاعبين</span>
+                <br />
+                <select className="bg-transparent border-b-2 my-1 w-12 focus:outline-none">
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                </select>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+
+        <Link href="/">
+          <h3 className="text-white mr-10 text-xl cursor-pointer float-right hover:text-black font-semibold">
+            الرئيسية <FaArrowRight className="inline ml-2" />
+          </h3>
+        </Link>
+        <Link href="/">
+          <h3 className="text-white ml-10 text-xl cursor-pointer float-left hover:text-black font-semibold">
+            <FaArrowLeft className="inline mr-2" />
+            التالي
+          </h3>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default WordCreate;
