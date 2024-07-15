d4027a955125fb65540ddf697cd362b31d3079ff
Karim Wael <devkarim@hotmail.com>
2024-07-15 21:52:19 +0300

    feat: add title to get started page

--- src/app/(routes)/(guest)/getstarted/page.tsx
+++ src/app/(routes)/(guest)/getstarted/page.tsx
@@ -1,3 +1,10 @@
+import { APP_NAME_EN } from '@/config/constants';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: `${APP_NAME_EN} | Get Started`,
+};
+
 export default function GetStarted() {
   return (
     <main className="flex flex-col items-center justiy-center p-12 gap-4">
