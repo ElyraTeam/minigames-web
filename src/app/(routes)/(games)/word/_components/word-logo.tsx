d7060dc6cc3316a1ad0f2463b5931e1a6d7ccff3
Karim Wael <devkarim@hotmail.com>
2024-07-15 21:38:25 +0300

    feat: add word-logo component

---
+++ src/app/(routes)/(games)/word/_components/word-logo.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+
+interface WordLogoProps {
+  size?: number;
+  onClick?: () => void;
+  className?: string;
+}
+
+const WordLogo: React.FC<WordLogoProps> = ({
+  size = 85,
+  className,
+  onClick,
+}) => {
+  return (
+    <Image
+      src="/wordlogo.svg"
+      width={size}
+      height={size}
+      alt="logo"
+      className={className}
+      onClick={onClick}
+    />
+  );
+};
+
+export default WordLogo;
