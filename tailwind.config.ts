8691a166a06db3b468a284185b75614bf4c72d87
Karim Wael <devkarim@hotmail.com>
2024-07-15 21:26:15 +0300

    feat: setup colors in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          100: '#52FA60',
+          200: '#41F950',
+          300: '#2FF940',
+          400: '#1EF830',
+          500: '#0DF822',
+          600: '#07EA1A',
+          700: '#07D818',
+          800: '#06C616',
+          900: '#047E0E',
+          DEFAULT: '#0DF822',
+        },
+        secondary: {
+          300: '#22B2B7',
+          500: '#1a8c90',
+          700: '#136366',
+          DEFAULT: '#1a8c90',
+        },
+        light: '#64EB97',
+        dark: '#4CD375',
+        'teamgradient-from': '#aa69cb',
+        'teamgradient-to': '#95aff3',
       },
     },
   },
