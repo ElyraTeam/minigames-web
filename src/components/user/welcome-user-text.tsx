c77f4981474f08307ae41d7630eadcedea9c2b5c
Karim Wael <devkarim@hotmail.com>
2024-07-15 21:35:25 +0300

    feat: add welcome-user-text component

---
+++ src/components/user/welcome-user-text.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+import UsernameText from './username-text';
+
+interface WelcomeUserTextProps {}
+
+const WelcomeUserText: React.FC<WelcomeUserTextProps> = ({}) => {
+  return (
+    <h1 className="text-3xl lg:text-5xl">
+      مرحبا <UsernameText />!
+      <Link href="/getstarted">
+        <span className="text-lg sm:text-xl px-2 cursor-pointer hover:text-primary">
+          (تغيير)
+        </span>
+      </Link>
+    </h1>
+  );
+};
+
+export default WelcomeUserText;
