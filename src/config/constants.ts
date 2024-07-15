ebd5062e34b6eb8f8005bcf8d0a85150b2a32f06
Karim Wael <devkarim@hotmail.com>
2024-07-15 21:31:24 +0300

    feat: add constants and utils

---
+++ src/config/constants.ts
@@ -0,0 +1,35 @@
+export const IS_TESTING = false;
+export const APP_NAME = 'ElyraGames';
+export const TEAM_NAME_AR = 'إليرا';
+export const TEAM_NAME_EN = 'Elyra';
+export const API_HOST = IS_TESTING
+  ? 'http://localhost:3000'
+  : `https://elyra-minis.hossamohsen.me/`;
+export const HOST = `https://elyragames.netlify.app`;
+
+export const CREDITS: UserCredit[] = [
+  {
+    name: 'كريم وائل',
+    facebookLink: 'https://www.facebook.com/kareem.wael.75',
+    linkedinLink: 'https://www.linkedin.com/in/devkarim',
+    githubLink: 'https://github.com/devkarim',
+  },
+  {
+    name: 'حسام محسن',
+    facebookLink: 'https://www.facebook.com/profile.php?id=100078325080840',
+    linkedinLink: 'https://www.linkedin.com/in/hossammohsen',
+    githubLink: 'https://github.com/7osCraft',
+  },
+  {
+    name: 'إياد خالد',
+    facebookLink: 'https://facebook.com/Eyad.Zinon77',
+    linkedinLink: 'https://www.linkedin.com/in/eyadkhaled',
+    githubLink: 'https://github.com/EyadKhaled',
+  },
+  {
+    name: 'محمد وليد',
+    facebookLink: 'https://www.facebook.com/profile.php?id=100016840150802',
+    linkedinLink: 'https://www.linkedin.com/in/mohamad-darwish-50ba59225',
+    githubLink: 'https://github.com/XJustMO',
+  },
+];
---
+++ src/lib/utils.ts
@@ -0,0 +1,26 @@
+import { twMerge } from 'tailwind-merge';
+import { type ClassValue, clsx } from 'clsx';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const shuffle = <T>(array: T[]): T[] => {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+};
