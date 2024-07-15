878b887c946929ee03ad570ffbeefbff306b395e
Karim Wael <devkarim@hotmail.com>
2024-07-15 21:38:57 +0300

    feat: add word-background component

---
+++ src/app/(routes)/(games)/word/_components/word-background.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+
+interface WordBackgroundProps {}
+
+const WordBackground: React.FC<WordBackgroundProps> = ({}) => {
+  return (
+    <div className="fixed w-full h-full top-0 left-0 -z-10">
+      <Image
+        src="/wordbackground.svg"
+        alt="word-background"
+        className="object-cover -z-10"
+        fill
+      />
+      <div className="bg-black w-full h-full opacity-30" />
+    </div>
+  );
+};
+
+export default WordBackground;
