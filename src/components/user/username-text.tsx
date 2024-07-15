9bf0fb1fdc127854b25667347d64985e8a9aa688
Karim Wael <devkarim@hotmail.com>
2024-07-15 21:34:29 +0300

    feat: setup username-text
    Setup username-text component that will contain local player's username.

---
+++ src/components/user/username-text.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+interface UsernameTextProps {}
+
+const UsernameText: React.FC<UsernameTextProps> = ({}) => {
+  return <span className="text-primary">{'username'}</span>;
+};
+
+export default UsernameText;
