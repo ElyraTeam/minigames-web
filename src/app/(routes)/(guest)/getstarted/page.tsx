56779b2dd2fefa766379428f93c15245fd881afb
Karim Wael <devkarim@hotmail.com>
2024-07-16 21:21:12 +0300

    feat: add get-started page

--- src/app/(routes)/(guest)/getstarted/page.tsx
+++ src/app/(routes)/(guest)/getstarted/page.tsx
@@ -1,5 +1,12 @@
-import { APP_NAME_EN } from '@/config/constants';
+import Link from 'next/link';
 import { Metadata } from 'next';
+
+import { APP_NAME_EN } from '@/config/constants';
+import WordLogo from '@/components/word/word-logo';
+import AuthorLinks from '@/components/about/author-links';
+import AuthorModal from '@/components/modals/author-modal';
+
+import NameInput from './_components/name-input';
 
 export const metadata: Metadata = {
   title: `${APP_NAME_EN} | Get Started`,
@@ -7,8 +14,18 @@ export const metadata: Metadata = {
 
 export default function GetStarted() {
   return (
-    <main className="flex flex-col items-center justiy-center p-12 gap-4">
-      <h2>ElyraGames GetStarted</h2>
+    <main className="flex flex-col items-center justify-center h-screen gap-24">
+      <div className="flex flex-col items-center gap-6">
+        <Link href="/word">
+          <WordLogo size={100} className="cursor-pointer" />
+        </Link>
+        <h2 className="text-2xl font-semibold">مرحبا بك في كلمة!</h2>
+      </div>
+      <NameInput />
+      <div className="flex flex-col items-center">
+        <AuthorModal className="text-lg">تطوير</AuthorModal>
+        <AuthorLinks />
+      </div>
     </main>
   );
 }
