993be1dac548eb5f9fb0c9a82c58ed924cdb44f5
Karim Wael <devkarim@hotmail.com>
2024-07-16 21:12:08 +0300

    feat: integrate username-text component to homepage

--- src/app/(routes)/(guest)/page.tsx
+++ src/app/(routes)/(guest)/page.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link';
 
+import UsernameText from '@/components/user/username-text';
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justiy-center p-12 gap-4">
       <div className="text-center">
         <h2>ElyraGames Homepage</h2>
         <h3>
-          Hello <span className="text-primary">Username</span>
+          اهلا <UsernameText />
         </h3>
       </div>
       <Link href="/word" className="text-primary text-2xl underline">
