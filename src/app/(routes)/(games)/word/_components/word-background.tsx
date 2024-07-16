f14ef83555ba4cf30b334f791ccb44031412a612
Karim Wael <devkarim@hotmail.com>
2024-07-16 20:46:40 +0300

    fix: change word-background svg path

--- src/app/(routes)/(games)/word/_components/word-background.tsx
+++ src/app/(routes)/(games)/word/_components/word-background.tsx
@@ -6,7 +6,7 @@ const WordBackground: React.FC<WordBackgroundProps> = ({}) => {
   return (
     <div className="fixed w-full h-full top-0 left-0 -z-10">
       <Image
-        src="/wordbackground.svg"
+        src="/svg/wordbackground.svg"
         alt="word-background"
         className="object-cover -z-10"
         fill
