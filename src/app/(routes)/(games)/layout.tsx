0eaa9f9015b39facafb369e5e679a81e02a57552
Karim Wael <devkarim@hotmail.com>
2024-07-16 22:53:14 +0300

    feat: allow only authorized users to navigate game pages
    Allow only users who have nicknames to navigate between game pages.

--- src/app/(routes)/(games)/layout.tsx
+++ src/app/(routes)/(games)/layout.tsx
@@ -1,14 +1,22 @@
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
 import Footer from '@/components/ui/footer';
+import NicknameProvider from '@/components/providers/nickname-provider';
 
 export default async function GamesLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const cookieStore = cookies();
+  const nickname = cookieStore.get('nextjs.session');
+  if (!nickname) redirect('/getstarted');
+
   return (
-    <>
+    <NicknameProvider nickname={nickname.value}>
       {children}
       <Footer />
-    </>
+    </NicknameProvider>
   );
 }
