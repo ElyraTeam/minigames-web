438e03f7a44f50d16820bb8449f70ad9b2d0f52f
Karim Wael <devkarim@hotmail.com>
2024-07-16 21:12:32 +0300

    fix: change create game link

--- src/app/(routes)/(games)/word/_components/word-home-buttons.tsx
+++ src/app/(routes)/(games)/word/_components/word-home-buttons.tsx
@@ -1,19 +1,21 @@
 import Link from 'next/link';
 
 import Button from '@/components/ui/button';
+import Button3D from '@/components/ui/button-3d';
 
 interface WordHomeButtonsProps {}
 
 const WordHomeButtons: React.FC<WordHomeButtonsProps> = ({}) => {
   return (
-    <div className="text-center flex flex-col sm:flex-row gap-6 justify-center">
-      <Link href="/word/room/create">
-        <Button>ابدأ لعبة جديدة</Button>
-      </Link>
-
-      <Link href="/">
-        <Button variant="outline">الواجهة الرئيسية</Button>
-      </Link>
+    <div className="text-center flex flex-col justify-center items-center">
+      <div className="flex flex-col gap-4 w-56">
+        <Link href="/word/create">
+          <Button3D>ابدأ اللعبة</Button3D>
+        </Link>
+        <Link href="/">
+          <Button variant="outline">الواجهة الرئيسية</Button>
+        </Link>
+      </div>
     </div>
   );
 };
