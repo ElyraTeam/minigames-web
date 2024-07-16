2e0e76c8bbc5b1464998e56c41bf239d9fa88e45
Karim Wael <devkarim@hotmail.com>
2024-07-16 20:44:48 +0300

    feat: add text variant and outline-gradient

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,13 +1,16 @@
 import { cn } from '@/lib/utils';
 import { cva, type VariantProps } from 'class-variance-authority';
 
-const buttonVariants = cva('text-xl px-12 py-3 rounded-2xl', {
+const buttonVariants = cva('text-xl px-8 py-3 rounded-xl w-full', {
   variants: {
     variant: {
       default:
-        'text-white bg-gradient-to-r from-primary-300 to-secondary-500 active:top-[2px] shadow-[0px_4px_0px_0px_rgba(0,0,0,.3)] active:shadow-[0px_2px_0px_0px_rgba(0,0,0,.3)] relative hover:opacity-90',
-      outline:
+        'text-white bg-gradient-to-r from-primary-300 to-secondary-300 active:top-[2px] shadow-[0px_4px_0px_0px_rgba(0,0,0,.3)] active:shadow-[0px_2px_0px_0px_rgba(0,0,0,.3)] relative hover:opacity-90',
+      'outline-gradient':
         'text-primary transition-all border-gradient hover:bg-primary-100/10',
+      outline:
+        'text-primary transition-all border-2 border-primary hover:bg-primary-100/10',
+      text: 'text-primary hover:text-primary/70 transition-all p-0',
     },
   },
   defaultVariants: {
