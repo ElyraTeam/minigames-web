5895c2ef5c2328f764b423f68e3013318ed3bfb9
Karim Wael <devkarim@hotmail.com>
2024-07-18 01:50:42 +0300

    feat: add word-side-card-header component

---
+++ src/app/(routes)/(games)/word/[gameId]/_components/word-side-card-header.tsx
@@ -0,0 +1,24 @@
+import { cn } from '@/lib/utils';
+
+interface WordSideCardHeaderProps {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+const WordSideCardHeader: React.FC<WordSideCardHeaderProps> = ({
+  children,
+  className,
+}) => {
+  return (
+    <div
+      className={cn(
+        'text-xl font-bold text-center p-6 bg-word-side-200 rounded-b-3xl shadow-[theme("colors.word.side.400")_0px_0px_0px_16px]',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default WordSideCardHeader;
