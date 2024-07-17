df041d77d4b2ac8df99953bf3cbe2848f4e2a01d
Karim Wael <devkarim@hotmail.com>
2024-07-18 01:50:08 +0300

    feat: add word-chat-message component
    Add word-chat-message component that includes player's message (content and sender).

---
+++ src/app/(routes)/(games)/word/[gameId]/_components/chat/word-chat-message.tsx
@@ -0,0 +1,31 @@
+import { cn } from '@/lib/utils';
+
+interface WordChatMessageProps {
+  sender?: string;
+  content: string;
+  self?: boolean;
+}
+
+const WordChatMessage: React.FC<WordChatMessageProps> = ({
+  sender,
+  content,
+  self,
+}) => {
+  return (
+    <div className={cn('flex flex-col gap-2', !self && 'items-end')}>
+      {!self && sender && (
+        <label className="text-sm opacity-60">{sender}</label>
+      )}
+      <p
+        className={cn(
+          'rounded-b-2xl rounded-tl-2xl px-4 py-2 bg-word-secondary w-fit text-wrap break-words max-w-full',
+          !self && 'bg-word-side-400 rounded-tl-none rounded-tr-2xl'
+        )}
+      >
+        {content}
+      </p>
+    </div>
+  );
+};
+
+export default WordChatMessage;
