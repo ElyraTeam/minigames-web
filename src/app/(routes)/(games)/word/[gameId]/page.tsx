952618758fc5d3e5ed6402b7552e7df6c5871147
Karim Wael <devkarim@hotmail.com>
2024-07-18 01:53:58 +0300

    feat: check if room is valid before joining word game
    Check if the room ID is valid before joining a word game room by checking server-side whether the room ID exists.

--- src/app/(routes)/(games)/word/[gameId]/page.tsx
+++ src/app/(routes)/(games)/word/[gameId]/page.tsx
@@ -1,15 +1,38 @@
+import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
+
+import { HOST } from '@/config/constants';
+import checkRoomId from '@/actions/check-room';
+
+import WordMainCard from './_components/word-main-card';
+
 interface WordRoomPageProps {
   params: {
     gameId: string;
   };
 }
 
-const WordRoomPage: React.FC<WordRoomPageProps> = ({ params: { gameId } }) => {
-  return (
-    <div>
-      <h1>Game Id: {gameId}</h1>
-    </div>
-  );
+export async function generateMetadata({
+  params: { gameId },
+}: WordRoomPageProps): Promise<Metadata> {
+  return {
+    description: 'Join this game of Word!',
+    openGraph: {
+      title: 'Join Word Game',
+      description: "You're invited to join this Word game!",
+      url: `${HOST}/word/${gameId}`,
+    },
+  };
+}
+
+const WordRoomPage: React.FC<WordRoomPageProps> = async ({
+  params: { gameId },
+}) => {
+  const isRoomValid = await checkRoomId(gameId);
+
+  if (!isRoomValid) redirect('/word/not-found');
+
+  return <WordMainCard roomId={gameId} />;
 };
 
 export default WordRoomPage;
