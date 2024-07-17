00184313d5cb16dbe790b9a39c9c52414af6c577
Karim Wael <devkarim@hotmail.com>
2024-07-18 01:43:18 +0300

    feat: add room not found page

---
+++ src/app/(routes)/(games)/word/not-found/page.tsx
@@ -0,0 +1,11 @@
+interface WordRoomNotFoundProps {}
+
+const WordRoomNotFound: React.FC<WordRoomNotFoundProps> = ({}) => {
+  return (
+    <div className="flex flex-col h-screen justify-center items-center text-4xl">
+      Room not found
+    </div>
+  );
+};
+
+export default WordRoomNotFound;
