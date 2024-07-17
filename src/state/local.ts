396bf73f67bd7967e3024853d991b994afe13736
Karim Wael <devkarim@hotmail.com>
2024-07-18 01:33:12 +0300

    feat: add token attribute to local store

--- src/state/local.ts
+++ src/state/local.ts
@@ -2,16 +2,20 @@ import { create } from 'zustand';
 import { devtools, persist } from 'zustand/middleware';
 
 interface LocalState {
-  nickname: string;
+  nickname: string | undefined | null;
+  token: string | undefined | null;
   setNickname: (nickname: string) => void;
+  setToken: (token: string) => void;
 }
 
 const useLocalStore = create<LocalState>()(
   devtools(
     persist(
       (set) => ({
-        nickname: '',
+        nickname: null,
+        token: null,
         setNickname: (nickname) => set(() => ({ nickname })),
+        setToken: (token) => set(() => ({ token })),
       }),
       {
         name: 'local-player-storage',
