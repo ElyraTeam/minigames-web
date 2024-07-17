9454b366b7b3a84e572e89c9523e6834b0b9c6c7
Karim Wael <devkarim@hotmail.com>
2024-07-18 01:42:43 +0300

    feat: add input component

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,36 @@
+import { cn } from '@/lib/utils';
+
+interface InputProps extends React.ComponentProps<'input'> {
+  icon?: React.ReactNode;
+  onIconClick?: () => any;
+}
+
+const Input: React.FC<InputProps> = ({
+  className,
+  icon,
+  onIconClick,
+  ...props
+}) => {
+  return (
+    <div className="relative">
+      <input
+        className={cn(
+          'rounded-lg bg-transparent outline-none border-2 border-word-side-200 w-full p-[8px] focus:border-word-secondary-300 transition-colors rtl:pe-10',
+          className
+        )}
+        {...props}
+      />
+      <div
+        className={cn(
+          'absolute inset-y-0 end-0 pe-3 flex items-center z-10',
+          onIconClick && 'cursor-pointer'
+        )}
+        onClick={onIconClick}
+      >
+        {icon}
+      </div>
+    </div>
+  );
+};
+
+export default Input;
