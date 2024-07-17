c2639681d7a15535e9c197f7576ee46a210da5b2
Karim Wael <devkarim@hotmail.com>
2024-07-18 01:51:38 +0300

    feat: add word-chat-container component
    Add word-chat-container component that includes chat's header and all room chat's messages.

---
+++ src/app/(routes)/(games)/word/[gameId]/_components/chat/word-chat-container.tsx
@@ -0,0 +1,26 @@
+import { MdMessage } from 'react-icons/md';
+
+import WordChatSend from './word-chat-send';
+import WordSideCard from '../word-side-card';
+import WordChatMessages from './word-chat-messages';
+import WordSideCardHeader from '../word-side-card-header';
+
+interface WordChatContainerProps {}
+
+const WordChatContainer: React.FC<WordChatContainerProps> = ({}) => {
+  return (
+    <WordSideCard className="flex flex-col">
+      <WordSideCardHeader className="space-x-1 rtl:space-x-reverse">
+        <span>المحادثة</span> <MdMessage className="text-2xl inline" />
+      </WordSideCardHeader>
+      <div className="flex flex-col mt-4 px-4 pt-10 pb-6 h-full overflow-y-auto scrollbar-thin">
+        <WordChatMessages />
+      </div>
+      <div className="py-4 px-4 border-t-2 border-white/10">
+        <WordChatSend />
+      </div>
+    </WordSideCard>
+  );
+};
+
+export default WordChatContainer;
