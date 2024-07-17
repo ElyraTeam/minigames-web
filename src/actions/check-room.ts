08cb564828edb3d4ccb9a231ac23990c85722774
Karim Wael <devkarim@hotmail.com>
2024-07-18 02:16:57 +0300

    feat: add nickname check before joining rooms
    Add nickname check before joining rooms to prevent duplicate names or same device joining more than once.

--- src/actions/check-room.ts
+++ src/actions/check-room.ts
@@ -1,8 +1,10 @@
 import { API_HOST } from '@/config/constants';
 
-const checkRoomId = async (roomId: string) => {
+const checkRoomId = async (roomId: string, nickname?: string) => {
   try {
-    const res = await fetch(`${API_HOST}/word/room/check/${roomId}`);
+    const res = await fetch(
+      `${API_HOST}/word/room/check/${roomId}?nickname=${nickname?.trim()}`
+    );
     return res.status == 200;
   } catch (err) {
     console.error(err);
