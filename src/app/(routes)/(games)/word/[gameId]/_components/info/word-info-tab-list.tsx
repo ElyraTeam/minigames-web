376d7b47fad4219419a2abae1819a35c2fa562ee
Karim Wael <devkarim@hotmail.com>
2024-07-18 22:19:21 +0300

    feat: add word-info-tab-list component
    Add word-info-tab-list component to navigate between current round score or total score.

---
+++ src/app/(routes)/(games)/word/[gameId]/_components/info/word-info-tab-list.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import TabItem from '@/components/ui/tab-item';
+import TabList from '@/components/ui/tab-list';
+
+import WordLeaderboard from './word-leaderboard';
+
+interface WordInfoTabListProps {}
+
+const WordInfoTabList: React.FC<WordInfoTabListProps> = ({}) => {
+  return (
+    <TabList
+      parentClassName="flex flex-col overflow-y-auto flex-grow scrollbar-thin"
+      className="mx-5 mt-10 mb-5 rounded-xl bg-word-side-400/40"
+      activeClassName="bg-word-side-200/80 rounded-xl shadow-md"
+      tabSwitchClassName="p-3"
+    >
+      <TabItem label="الحالية" className="overflow-y-auto">
+        <WordLeaderboard lastRound />
+      </TabItem>
+      <TabItem label="المتصدرون" className="overflow-y-auto">
+        <WordLeaderboard />
+      </TabItem>
+    </TabList>
+  );
+};
+
+export default WordInfoTabList;
