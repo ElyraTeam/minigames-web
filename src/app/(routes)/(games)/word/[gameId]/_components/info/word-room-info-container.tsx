94b536cbfb83cec37c15eb39b323d44687621f84
Karim Wael <devkarim@hotmail.com>
2024-07-18 22:20:53 +0300

    feat: display actual round and players info
    Display actual current round and game's players info.

--- src/app/(routes)/(games)/word/[gameId]/_components/info/word-room-info-container.tsx
+++ src/app/(routes)/(games)/word/[gameId]/_components/info/word-room-info-container.tsx
@@ -1,22 +1,14 @@
-import { MdRefresh } from 'react-icons/md';
-
-import IconButton from '@/components/ui/icon-button';
-
 import WordSideCard from '../word-side-card';
-import WordSideCardHeader from '../word-side-card-header';
+import WordInfoTabList from './word-info-tab-list';
+import WordCurrentRound from './word-current-round';
 
 interface WordRoomInfoContainerProps {}
 
 const WordRoomInfoContainer: React.FC<WordRoomInfoContainerProps> = ({}) => {
   return (
-    <WordSideCard>
-      <WordSideCardHeader className="space-x-3 rtl:space-x-reverse">
-        <span>الجولة</span>
-        <span>12/0</span>
-        <IconButton>
-          <MdRefresh className="inline text-3xl" />
-        </IconButton>
-      </WordSideCardHeader>
+    <WordSideCard className="flex flex-col">
+      <WordCurrentRound />
+      <WordInfoTabList />
     </WordSideCard>
   );
 };
