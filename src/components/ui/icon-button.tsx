0d473965a33a4ac8d148b84f4a88c3778925e30b
Karim Wael <devkarim@hotmail.com>
2024-07-18 22:16:16 +0300

    refactor: change default size of icon-button component

--- src/components/ui/icon-button.tsx
+++ src/components/ui/icon-button.tsx
@@ -6,7 +6,7 @@ const IconButton: React.FC<IconButtonProps> = ({ className, ...props }) => {
   return (
     <span
       className={cn(
-        'bg-gray-400/30 w-10 h-10 inline-block rounded-full cursor-pointer hover:bg-gray-400/50 transition-colors content-center',
+        'bg-gray-400/30 w-8 h-8 inline-block rounded-full cursor-pointer hover:bg-gray-400/50 transition-colors content-center',
         className
       )}
       {...props}
