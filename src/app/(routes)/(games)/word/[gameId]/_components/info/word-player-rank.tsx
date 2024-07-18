1255b739e9d2cde110538118d6b30dedfa44d11b
Karim Wael <devkarim@hotmail.com>
2024-07-18 22:17:37 +0300

    feat: add word-player-rank component
    Add word-player-rank component to display player's name, score, and whether they're online or owner.

---
+++ src/app/(routes)/(games)/word/[gameId]/_components/info/word-player-rank.tsx
@@ -0,0 +1,60 @@
+import { FaCrown } from 'react-icons/fa';
+
+import { cn } from '@/lib/utils';
+
+interface WordPlayerRankProps {
+  rank: number;
+  name?: string;
+  score?: number;
+  isOwner?: boolean;
+  isLocalPlayer?: boolean;
+  isOnline?: boolean;
+}
+
+const WordPlayerRank: React.FC<WordPlayerRankProps> = ({
+  rank,
+  name,
+  score,
+  isOwner,
+  isLocalPlayer,
+  isOnline,
+}) => {
+  const isOdd = rank % 2 == 1;
+
+  return (
+    <div
+      className={cn(
+        'py-6 px-4 flex items-center',
+        isOdd && 'bg-word-secondary-700/15',
+        isLocalPlayer && 'text-word-game',
+        !isOnline && 'text-white/20'
+      )}
+    >
+      <div className="flex items-center gap-3 flex-grow">
+        <p
+          className={cn(
+            'w-6 h-6 rounded-full text-center text-white/80',
+            rank == 1 && 'bg-yellow-600 text-black',
+            rank == 2 && 'bg-white text-black',
+            rank == 3 && 'bg-yellow-700 text-black'
+          )}
+        >
+          {rank}
+        </p>
+        <p className="text-lg overflow-hidden overflow-ellipsis max-w-[6.8rem] whitespace-nowrap font-semibold">
+          {name || '------'}
+        </p>
+        {isOwner && (
+          <span className="flex flex-col justify-center items-center bg-white w-5 h-5 rounded-full">
+            <FaCrown className="text-sm text-yellow-500" />
+          </span>
+        )}
+      </div>
+      <div className="border-r-2 border-white/20 w-12 text-center" dir="ltr">
+        <p className="font-semibold text-sm">{score || 0}</p>
+      </div>
+    </div>
+  );
+};
+
+export default WordPlayerRank;
