953a3f5fa788e2faf52c882c48f98a4da7ee4736
Karim Wael <devkarim@hotmail.com>
2024-07-19 03:55:31 +0300

    feat: add word-game-content component

---
+++ src/app/(routes)/(games)/word/[gameId]/_components/word-game-content.tsx
@@ -0,0 +1,20 @@
+import { cn } from '@/lib/utils';
+
+interface WordGameContentProps extends React.ComponentProps<'div'> {}
+
+const WordGameContent: React.FC<WordGameContentProps> = ({
+  className,
+  ...props
+}) => {
+  return (
+    <div
+      className={cn(
+        'flex flex-col w-full h-full shadow-[inset_0_0_10px_rgba(0,0,0,0.2)] rounded-2xl bg-word-game-600 mb-4 py-8 px-3',
+        className
+      )}
+      {...props}
+    />
+  );
+};
+
+export default WordGameContent;
