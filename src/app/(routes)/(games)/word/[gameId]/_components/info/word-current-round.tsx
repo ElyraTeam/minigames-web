99a45df008b81d32b03bbba34e97750be7026c26
Karim Wael <devkarim@hotmail.com>
2024-07-19 03:57:30 +0300

    feat: add reset game functionality
    Add reset game functionality and a tooltip for its icon button.

--- src/app/(routes)/(games)/word/[gameId]/_components/info/word-current-round.tsx
+++ src/app/(routes)/(games)/word/[gameId]/_components/info/word-current-round.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import toast from 'react-hot-toast';
 import { MdRefresh } from 'react-icons/md';
 
 import useGameStore from '@/state/game';
 import useRoomStore from '@/state/room';
 import IconButton from '@/components/ui/icon-button';
+
+import localPlayer from '@/api/socket';
 
 import WordSideCardHeader from '../word-side-card-header';
 
@@ -13,6 +16,11 @@ interface WordCurrentRoundProps {}
 const WordCurrentRound: React.FC<WordCurrentRoundProps> = ({}) => {
   const gameState = useGameStore((state) => state.game);
   const roomOptions = useRoomStore((state) => state.options);
+
+  const handleReset = () => {
+    localPlayer.resetGame();
+    toast.success('تم اعادة اللعبة.', { id: 'word-reset-game' });
+  };
 
   return (
     <WordSideCardHeader className="flex items-center justify-center space-x-3 rtl:space-x-reverse">
@@ -23,8 +31,8 @@ const WordCurrentRound: React.FC<WordCurrentRoundProps> = ({}) => {
         </span>
         {gameState?.currentRound || '?'}
       </span>
-      <IconButton>
-        <MdRefresh className="inline text-2xl mb-3" />
+      <IconButton tooltip="اعادة اللعبة" onClick={handleReset}>
+        <MdRefresh className="inline text-2xl" />
       </IconButton>
     </WordSideCardHeader>
   );
