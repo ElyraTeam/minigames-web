fd86ee59538b4aa011d8539e02f7a44eee83b5b4
Karim Wael <devkarim@hotmail.com>
2024-07-19 23:06:54 +0300

    feat: add word-setting-header component

---
+++ src/app/(routes)/(games)/word/[gameId]/_components/lobby/word-setting-header.tsx
@@ -0,0 +1,27 @@
+import { cn } from '@/lib/utils';
+
+interface WordSettingHeaderProps extends React.ComponentProps<'div'> {
+  title: string;
+}
+
+const WordSettingHeader: React.FC<WordSettingHeaderProps> = ({
+  title,
+  className,
+  children,
+  ...props
+}) => {
+  return (
+    <div
+      className={cn(
+        'flex items-center justify-between py-3 px-14 rounded-full shadow-inner bg-word-game-700/40',
+        className
+      )}
+      {...props}
+    >
+      <p className="text-lg font-semibold">{title}</p>
+      {children}
+    </div>
+  );
+};
+
+export default WordSettingHeader;
