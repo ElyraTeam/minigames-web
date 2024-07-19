90243508d09dad0c1fb0b4b2ae3ba3ed6d02a4fa
Karim Wael <devkarim@hotmail.com>
2024-07-19 03:57:57 +0300

    feat: add word-game-header component
    Add word-game-header component that shows word's logo and action icon buttons.

---
+++ src/app/(routes)/(games)/word/[gameId]/_components/word-game-header.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+import WordLogo from '@/components/word/word-logo';
+
+import WordGameIcons from './word-game-icons';
+
+interface WordGameHeaderProps {}
+
+const WordGameHeader: React.FC<WordGameHeaderProps> = ({}) => {
+  return (
+    <div className="flex justify-center items-center gap-3 py-5 relative">
+      <WordGameIcons />
+      <div className="absolute right-0">
+        <Link href="/word">
+          <WordLogo />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default WordGameHeader;
