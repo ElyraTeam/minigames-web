e39bfe70540ec0edb6a3c7ee3ce39048ec8e3156
Karim Wael <devkarim@hotmail.com>
2024-07-19 23:11:22 +0300

    feat: display word general settings and letter settings

--- src/app/(routes)/(games)/word/[gameId]/_components/lobby/word-game-settings.tsx
+++ src/app/(routes)/(games)/word/[gameId]/_components/lobby/word-game-settings.tsx
@@ -1,7 +1,19 @@
+import WordGameContainer from '../word-game-container';
+import WordCategoryHeader from './word-category-header';
+import WordLetterSettings from './word-letter-settings';
+import WordGeneralSettings from './word-general-settings';
+
 interface WordGameSettingsProps {}
 
 const WordGameSettings: React.FC<WordGameSettingsProps> = ({}) => {
-  return <div></div>;
+  return (
+    <WordGameContainer>
+      <WordCategoryHeader>الإعدادات العامة</WordCategoryHeader>
+      <WordGeneralSettings />
+      <WordCategoryHeader>تخصيص اللعبة</WordCategoryHeader>
+      <WordLetterSettings />
+    </WordGameContainer>
+  );
 };
 
 export default WordGameSettings;
