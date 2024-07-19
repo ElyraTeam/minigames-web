a02e9dc186bb64f6c900bdca91a29ad168d6574f
Karim Wael <devkarim@hotmail.com>
2024-07-19 03:52:42 +0300

    feat: integrate tooltip with icon-button component

--- src/components/ui/icon-button.tsx
+++ src/components/ui/icon-button.tsx
@@ -1,16 +1,26 @@
 import { cn } from '@/lib/utils';
 
-interface IconButtonProps extends React.ComponentProps<'span'> {}
+import Tooltip from './tooltip';
 
-const IconButton: React.FC<IconButtonProps> = ({ className, ...props }) => {
+interface IconButtonProps extends React.ComponentProps<'span'> {
+  tooltip?: string;
+}
+
+const IconButton: React.FC<IconButtonProps> = ({
+  className,
+  tooltip,
+  ...props
+}) => {
   return (
-    <span
-      className={cn(
-        'bg-gray-400/30 w-8 h-8 inline-block rounded-full cursor-pointer hover:bg-gray-400/50 transition-colors content-center',
-        className
-      )}
-      {...props}
-    />
+    <Tooltip text={tooltip}>
+      <span
+        className={cn(
+          'bg-gray-400/30 w-8 h-8 rounded-full cursor-pointer hover:bg-gray-400/50 transition-colors flex flex-col items-center justify-center',
+          className
+        )}
+        {...props}
+      />
+    </Tooltip>
   );
 };
 
