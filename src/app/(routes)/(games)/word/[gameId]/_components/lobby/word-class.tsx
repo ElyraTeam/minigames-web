6df82eb9df3d87fb74f3ad1cf0a2e4a4c21d6131
Karim Wael <devkarim@hotmail.com>
2024-07-20 23:46:26 +0300

    feat: add word-class component

---
+++ src/app/(routes)/(games)/word/[gameId]/_components/lobby/word-class.tsx
@@ -0,0 +1,24 @@
+import { IoClose } from 'react-icons/io5';
+
+interface WordClassProps {
+  name: string;
+  onDelete: () => void;
+}
+
+const WordClass: React.FC<WordClassProps> = ({ name, onDelete }) => {
+  return (
+    <div className="relative bg-white/10 border-2 border-white rounded-lg w-28 text-center px-2 py-1">
+      <p className="overflow-hidden overflow-ellipsis whitespace-nowrap text-lg">
+        {name}
+      </p>
+      <span
+        className="flex flex-col items-center justify-center absolute rounded-full -top-2 -right-2 w-5 h-5 bg-danger cursor-pointer"
+        onClick={onDelete}
+      >
+        <IoClose className="text-lg" />
+      </span>
+    </div>
+  );
+};
+
+export default WordClass;
