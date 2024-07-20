41578d021c65e9b43e798b2f841abadf103e30d3
Karim Wael <devkarim@hotmail.com>
2024-07-20 21:53:47 +0300

    fix: resolve word-letter taking more space than it should be

--- src/app/(routes)/(games)/word/[gameId]/_components/lobby/word-letter.tsx
+++ src/app/(routes)/(games)/word/[gameId]/_components/lobby/word-letter.tsx
@@ -10,11 +10,11 @@ const WordLetter: React.FC<WordLetterProps> = ({
   ...props
 }) => {
   return (
-    <label>
+    <label className="relative w-12 h-12">
       <input type="checkbox" className="peer opacity-0 w-0 h-0" {...props} />
       <span
         className={cn(
-          'flex flex-col items-center justify-center bg-white w-12 h-12 rounded-full text-black cursor-pointer peer-checked:bg-word-secondary transition-colors peer-checked:text-white text-lg',
+          'absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center bg-white w-12 h-12 rounded-full text-black cursor-pointer peer-checked:bg-word-secondary transition-colors peer-checked:text-white text-lg peer-checked:peer-disabled:!bg-word-secondary/70 peer-disabled:cursor-default',
           className
         )}
       >
