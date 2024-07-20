1ef8c8bccab42cb9ab10488dea6beba8ca8f007a
Karim Wael <devkarim@hotmail.com>
2024-07-20 19:25:07 +0300

    refactor: reverse orientation of switch

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -10,7 +10,7 @@ const Switch: React.FC<SwitchProps> = ({ className, ...props }) => {
       <input type="checkbox" className="peer opacity-0 w-0 h-0" {...props} />
       <span
         className={cn(
-          "absolute bg-slate-300/50 top-0 left-0 right-0 bottom-0 cursor-pointer transition-all duration-300 rounded-full before:content-[''] before:absolute before:transition-all before:duration-300 before:rounded-full before:h-[75%] before:bg-white before:peer-checked:translate-x-0 before:aspect-square before:top-1/2 before:-translate-y-1/2 before:left-1/2 before:-translate-x-full",
+          "absolute bg-slate-300/50 top-0 left-0 right-0 bottom-0 cursor-pointer transition-all duration-300 rounded-full before:content-[''] before:absolute before:transition-all before:duration-300 before:rounded-full before:h-[75%] before:bg-white before:peer-checked:-translate-x-full before:aspect-square before:top-1/2 before:-translate-y-1/2 before:left-1/2",
           className
         )}
       />
