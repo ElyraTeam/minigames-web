60b76058433e027dc6edfc4794c28bda0214b36d
Karim Wael <devkarim@hotmail.com>
2024-07-20 23:47:19 +0300

    feat: add word-select-classes component
    Add word-select-classes component that displays all specified classes with the ability to delete each one.

---
+++ src/app/(routes)/(games)/word/[gameId]/_components/lobby/word-select-classes.tsx
@@ -0,0 +1,25 @@
+import WordClass from './word-class';
+
+interface WordSelectClassesProps {
+  classes: string[];
+  onDelete: (className: string) => void;
+}
+
+const WordSelectClasses: React.FC<WordSelectClassesProps> = ({
+  classes,
+  onDelete,
+}) => {
+  return (
+    <div className="flex justify-center flex-wrap gap-5 px-4">
+      {classes.map((className) => (
+        <WordClass
+          key={`word-class-${className}`}
+          name={className}
+          onDelete={() => onDelete(className)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default WordSelectClasses;
