538f1578a8a14d521a5e3b7b2b597aef9f1baa83
Karim Wael <devkarim@hotmail.com>
2024-07-20 20:30:16 +0300

    feat: add updateRoom function to room state

--- src/state/room.ts
+++ src/state/room.ts
@@ -4,12 +4,15 @@ import { devtools } from 'zustand/middleware';
 interface RoomState {
   options?: GameOptionsSync | null;
   setRoom: (options: GameOptionsSync) => void;
+  updateRoom: (options: RoomOptions) => void;
 }
 
 const useRoomStore = create<RoomState>()(
   devtools((set) => ({
     options: null,
     setRoom: (options) => set(() => ({ options })),
+    updateRoom: (options) =>
+      set((state) => ({ options: { ...state.options, options } })),
   }))
 );
 
