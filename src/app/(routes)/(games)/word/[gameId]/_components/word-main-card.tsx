a411e486a62f7e52ef05979733a19bd1aa8a25ea
Karim Wael <devkarim@hotmail.com>
2024-07-26 00:26:42 +0300

    feat: setup displaying game content based on state
    Setup displaying either settings, countdown, or game content based on current game's state.

--- src/app/(routes)/(games)/word/[gameId]/_components/word-main-card.tsx
+++ src/app/(routes)/(games)/word/[gameId]/_components/word-main-card.tsx
@@ -1,27 +1,70 @@
 'use client';
 
+import { State } from '@/types/word';
+import useGameStore from '@/state/game';
 import useCurrentGame from '@/hooks/use-current-game';
 
 import WordCard from './word-card';
+import WordCountdown from './word-countdown';
 import WordGameHeader from './word-game-header';
 import WordGameContent from './word-game-content';
+import WordDoneButton from './game/word-done-button';
 import WordReadyButton from './lobby/word-ready-button';
 import WordGameSettings from './lobby/word-game-settings';
+import WordGame from './game/word-game';
 
 interface WordMainCardProps {
   roomId: string;
 }
 
 const WordMainCard: React.FC<WordMainCardProps> = ({ roomId }) => {
-  useCurrentGame(roomId);
+  const game = useGameStore((state) => state.game);
+  const { countdown } = useCurrentGame(roomId);
+
+  const renderContentFromState = () => {
+    if (game && countdown) {
+      if (game.state === State.VOTING) {
+        return (
+          <WordCountdown
+            title={
+              <>
+                انتهي{' '}
+                <span className="text-word-secondary-500/40">
+                  {game.stopClicker || 'شخص'}
+                </span>{' '}
+                من الكتابة!
+              </>
+            }
+            subtitle="سيبدأ التصويت بعد:"
+            countdown={countdown}
+          />
+        );
+      }
+      return (
+        <WordCountdown
+          title="استعد للكتابة!"
+          subtitle="ستبدأ الجولة بعد:"
+          countdown={countdown}
+        />
+      );
+    }
+    if (!game || game.state === State.LOBBY) return <WordGameSettings />;
+    if (game.state === State.INGAME) return <WordGame />;
+  };
+
+  const renderButtonFromState = () => {
+    if (countdown) return null;
+    if (!game || game.state === State.LOBBY) return <WordReadyButton />;
+    if (game.state === State.VOTING || game.state === State.INGAME)
+      return <WordDoneButton state={game.state} />;
+    return null;
+  };
 
   return (
     <WordCard className="flex flex-col bg-word-game py-3 px-6 gap-3 overflow-y-auto scrollbar-thin">
       <WordGameHeader />
-      <WordGameContent>
-        <WordGameSettings />
-      </WordGameContent>
-      <WordReadyButton />
+      <WordGameContent>{renderContentFromState()}</WordGameContent>
+      {renderButtonFromState()}
     </WordCard>
   );
 };
