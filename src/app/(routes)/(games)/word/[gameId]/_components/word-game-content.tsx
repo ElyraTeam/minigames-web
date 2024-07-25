254934179e74aa90021e939b08a53a207eaf0162
Karim Wael <devkarim@hotmail.com>
2024-07-26 00:24:02 +0300

    feat: add maximum height to word-game-content

--- src/app/(routes)/(games)/word/[gameId]/_components/word-game-content.tsx
+++ src/app/(routes)/(games)/word/[gameId]/_components/word-game-content.tsx
@@ -9,7 +9,7 @@ const WordGameContent: React.FC<WordGameContentProps> = ({
   return (
     <div
       className={cn(
-        'flex flex-col w-full h-full shadow-[inset_0_0_10px_rgba(0,0,0,0.2)] rounded-2xl bg-word-game-600 mb-4 overflow-y-auto scrollbar-thin border-8 border-word-game',
+        'flex flex-col w-full h-full max-h-96 shadow-[inset_0_0_10px_rgba(0,0,0,0.2)] rounded-2xl bg-word-game-600 mb-4 overflow-y-auto scrollbar-thin border-8 border-word-game',
         className
       )}
       {...props}
