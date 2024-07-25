8ecb7ba915b82f1d50f3a47dbb543716f953f7d2
Karim Wael <devkarim@hotmail.com>
2024-07-26 00:49:18 +0300

    feat: add onChange func to word-category-box

--- src/app/(routes)/(games)/word/[gameId]/_components/game/word-category-box.tsx
+++ src/app/(routes)/(games)/word/[gameId]/_components/game/word-category-box.tsx
@@ -3,15 +3,21 @@ import Input from '@/components/ui/input';
 interface WordCategoryBoxProps {
   label: string;
   letter: string;
+  onChange?: (value: string) => void;
 }
 
-const WordCategoryBox: React.FC<WordCategoryBoxProps> = ({ label, letter }) => {
+const WordCategoryBox: React.FC<WordCategoryBoxProps> = ({
+  label,
+  letter,
+  onChange,
+}) => {
   return (
     <div className="space-y-2 text-center">
       <label className="text-lg">{label}</label>
       <Input
         className="placeholder:text-white/60 bg-white/[0.275] text-white border-transparent focus:border-word-primary-900 overflow-y-hidden px-4"
         placeholder={`${letter}ـ..`}
+        onChange={(e) => onChange?.(e.target.value)}
       />
     </div>
   );
