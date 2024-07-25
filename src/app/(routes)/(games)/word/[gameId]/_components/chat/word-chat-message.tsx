e14f3bc48d72c01f95716e444307720db7793dab
Karim Wael <devkarim@hotmail.com>
2024-07-26 00:34:53 +0300

    feat: add rounded and spacing props to word-chat-message

--- src/app/(routes)/(games)/word/[gameId]/_components/chat/word-chat-message.tsx
+++ src/app/(routes)/(games)/word/[gameId]/_components/chat/word-chat-message.tsx
@@ -4,22 +4,30 @@ interface WordChatMessageProps {
   sender?: string;
   content: string;
   self?: boolean;
+  rounded?: boolean;
+  spacing?: number;
 }
 
 const WordChatMessage: React.FC<WordChatMessageProps> = ({
   sender,
   content,
   self,
+  rounded = true,
+  spacing = 10,
 }) => {
   return (
-    <div className={cn('flex flex-col gap-2', !self && 'items-end')}>
+    <div
+      className={cn('flex flex-col gap-2', !self && 'items-end')}
+      style={{ marginTop: spacing }}
+    >
       {!self && sender && (
         <label className="text-sm opacity-60">{sender}</label>
       )}
       <p
         className={cn(
           'rounded-b-2xl rounded-tl-2xl px-4 py-2 bg-word-secondary w-fit text-wrap break-words max-w-full text-[14px] select-text',
-          !self && 'bg-word-side-400 rounded-tl-none rounded-tr-2xl'
+          !self && 'bg-word-side-400 rounded-tl-none rounded-tr-2xl',
+          !rounded && 'rounded-tr-2xl rounded-tl-2xl'
         )}
       >
         {content}
