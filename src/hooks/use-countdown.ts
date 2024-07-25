0130266468444b8dd4c2ba123047c8e1f8c400e9
Karim Wael <devkarim@hotmail.com>
2024-07-26 00:16:28 +0300

    feat: add use-countdown hook

---
+++ src/hooks/use-countdown.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+
+interface UseCountdownProps {
+  startFrom: number;
+  onCountdownUpdate?: (s: number) => void;
+  onCountdownFinish?: () => void;
+}
+
+const useCountdown = ({
+  startFrom,
+  onCountdownUpdate,
+  onCountdownFinish,
+}: UseCountdownProps) => {
+  const [countdown, setCountdown] = useState(startFrom);
+
+  useEffect(() => {
+    const startLocalTimer = () => {
+      setTimeout(() => {
+        const newCountdown = countdown! - 1;
+        setCountdown(newCountdown);
+        if (newCountdown == 0) {
+          onCountdownFinish && onCountdownFinish();
+        }
+      }, 1000);
+    };
+
+    if (countdown != 0) {
+      onCountdownUpdate && onCountdownUpdate(countdown);
+      startLocalTimer();
+    }
+  }, [countdown, onCountdownFinish, onCountdownUpdate]);
+
+  return { countdown, setCountdown };
+};
+
+export default useCountdown;
