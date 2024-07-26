5fb094da3fb44e545eff7a090d3fd736584f0c21
Karim Wael <devkarim@hotmail.com>
2024-07-26 22:12:02 +0300

    refactor: center tooltip

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -3,7 +3,7 @@ import { cn } from '@/lib/utils';
 interface TooltipProps {
   children?: React.ReactNode;
   className?: string;
-  text?: string;
+  text?: React.ReactNode;
   position?: TooltipPosition;
 }
 
@@ -14,15 +14,15 @@ const Tooltip: React.FC<TooltipProps> = ({
   position = 'bottom',
 }) => {
   return (
-    <div className={'relative inline-block'}>
+    <div className="relative">
       <div className="group">
         {children}
         {text && (
           <div
             className={cn(
-              'absolute bg-gray-700 hidden group-hover:block whitespace-nowrap py-2 px-4 rounded-xl text-white -left-[30px] text-base font-normal',
+              'absolute bg-gray-700 hidden group-hover:block whitespace-nowrap py-2 px-4 rounded-xl text-white -translate-x-1/2 left-1/2 text-base font-normal z-10',
               position === 'top' && 'top-[-50px]',
-              position == 'bottom' && 'bottom-[-50px]',
+              position === 'bottom' && 'mt-[20%]',
               className
             )}
           >
