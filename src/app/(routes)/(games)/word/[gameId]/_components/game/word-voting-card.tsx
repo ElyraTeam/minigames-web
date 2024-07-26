01c5a17ee99df078e3922272c68483b5de001067
Karim Wael <devkarim@hotmail.com>
2024-07-26 22:14:42 +0300

    feat: add word-voting-card component

---
+++ src/app/(routes)/(games)/word/[gameId]/_components/game/word-voting-card.tsx
@@ -0,0 +1,83 @@
+import { cn } from '@/lib/utils';
+import useVoteStore from '@/state/vote';
+import { getPlayerById } from '@/lib/word';
+import Tooltip from '@/components/ui/tooltip';
+import usePlayersStore from '@/state/players';
+import { availableVotes, Vote } from '@/config/word';
+
+interface WordVotingCardProps {
+  playerId: string;
+  name: string;
+  value: string;
+  vote?: Vote;
+  selectedCard?: boolean;
+  onChangeVote: (vote: Vote) => any;
+}
+
+const WordVotingCard: React.FC<WordVotingCardProps> = ({
+  playerId,
+  name,
+  value,
+  vote,
+  selectedCard,
+  onChangeVote,
+}) => {
+  const playerVotes = useVoteStore((state) => state.votes);
+  const players = usePlayersStore((state) => state.players?.players) || [];
+  const valueVotes = playerVotes?.[playerId];
+
+  console.log(playerVotes);
+  console.log(name, valueVotes);
+
+  const getVotesToPlayers = () => {
+    const allVotes: { [vote: number]: string[] } = {};
+    for (const plrId in valueVotes) {
+      const vote = valueVotes[plrId];
+      const playerName = getPlayerById(players, plrId)?.nickname;
+      if (playerName) {
+        allVotes[vote] = [...(allVotes[vote] || []), playerName];
+      }
+    }
+    return allVotes;
+  };
+
+  const votesToPlayers = getVotesToPlayers();
+
+  return (
+    <div
+      className={cn(
+        'bg-word-game/70 py-4 px-12 rounded-2xl space-y-8 text-center shadow-lg',
+        selectedCard && 'bg-word-secondary/70'
+      )}
+    >
+      <p className="font-light text-white/70 leading-none">{name}</p>
+      <h3 className="font-semibold leading-none">{value}</h3>
+      <div className="flex flex-row rtl:flex-row-reverse justify-center items-center gap-6">
+        {availableVotes.map((voteNumber) => (
+          <Tooltip
+            text={
+              votesToPlayers[voteNumber]
+                ? votesToPlayers[voteNumber].join(' | ')
+                : undefined
+            }
+            key={`word-voting-card-${name}-${voteNumber}`}
+          >
+            <div
+              className={cn(
+                'flex items-center justify-center bg-white/20 rounded-full w-8 h-8 hover:bg-white/30 transition-colors font-semibold cursor-pointer',
+                !selectedCard &&
+                  vote === voteNumber &&
+                  'border-2 bg-word-secondary hover:bg-word-secondary/80'
+              )}
+              onClick={() => onChangeVote(voteNumber)}
+            >
+              <p>{voteNumber}</p>
+            </div>
+          </Tooltip>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default WordVotingCard;
