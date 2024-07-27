297e0b74122276d827c5ce61f92d532789d2e6bb
Karim Wael <devkarim@hotmail.com>
2024-07-27 19:27:33 +0300

    refactor: setup word game layout for responsiveness

--- src/app/(routes)/(games)/word/[gameId]/layout.tsx
+++ src/app/(routes)/(games)/word/[gameId]/layout.tsx
@@ -1,4 +1,4 @@
-import WordChatContainer from './_components/chat/word-chat-container';
+import WordChat from './_components/chat/word-chat';
 import WordRoomInfoContainer from './_components/info/word-room-info-container';
 
 export default async function WordGameLayout({
@@ -7,10 +7,10 @@ export default async function WordGameLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="grid grid-cols-[275px_minmax(600px,1fr)_275px] h-screen justify-center items-stretch gap-6 py-12 px-6">
+    <div className="grid grid-rows-[600px_600px] grid-cols-none lg:grid-rows-none lg:grid-cols-[minmax(200px,275px)_minmax(550px,1fr)_minmax(200px,275px)] lg:h-screen lg:justify-center items-stretch gap-4 py-8 px-3 lg:py-12 lg:px-6">
       <WordRoomInfoContainer />
       {children}
-      <WordChatContainer />
+      <WordChat />
     </div>
   );
 }
