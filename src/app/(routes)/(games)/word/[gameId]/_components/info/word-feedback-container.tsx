9c846afbf9363ab7526a58ed655f628eb0d9a902
Karim Wael <devkarim@hotmail.com>
2024-07-28 02:00:06 +0300

    feat: add word-feedback component
    Add word-feedback component that displays a form to send feedback to authors.

--- src/app/(routes)/(games)/word/[gameId]/_components/info/word-feedback-container.tsx
+++ src/app/(routes)/(games)/word/[gameId]/_components/info/word-feedback-container.tsx
@@ -1,15 +1,30 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
+import WordFeedback from './word-feedback';
 
 interface WordFeedbackContainerProps {}
 
 const WordFeedbackContainer: React.FC<WordFeedbackContainerProps> = ({}) => {
+  const [feedbackModalShow, setFeedbackModalShow] = useState(false);
+
   return (
-    <div className="py-8 px-3 text-center font-light flex gap-2 justify-center text-sm shadow-[0px_-10px_10px_rgba(0,0,0,0.1)]">
-      <p>واجهتك مشكلة؟</p>
-      <Link href="/" className="underline text-word-game">
-        تواصل معنا
-      </Link>
-    </div>
+    <>
+      <WordFeedback
+        isOpen={feedbackModalShow}
+        onClose={() => setFeedbackModalShow(false)}
+      />
+      <div className="py-8 px-3 text-center font-light flex gap-2 justify-center text-sm shadow-[0px_-10px_10px_rgba(0,0,0,0.1)]">
+        <p>واجهتك مشكلة؟</p>
+        <p
+          className="underline text-word-game cursor-pointer"
+          onClick={() => setFeedbackModalShow(true)}
+        >
+          تواصل معنا
+        </p>
+      </div>
+    </>
   );
 };
 
