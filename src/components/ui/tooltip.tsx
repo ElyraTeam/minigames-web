c1eee9a71cecad29531f3b84a45363feaf7dd5e7
Karim Wael <devkarim@hotmail.com>
2024-07-27 17:53:58 +0300

    refactor: remove redundant div from tooltip

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -14,22 +14,20 @@ const Tooltip: React.FC<TooltipProps> = ({
   position = 'bottom',
 }) => {
   return (
-    <div className="relative">
-      <div className="group">
-        {children}
-        {text && (
-          <div
-            className={cn(
-              'absolute bg-gray-700 hidden group-hover:block whitespace-nowrap py-2 px-4 rounded-xl text-white -translate-x-1/2 left-1/2 text-base font-normal z-10',
-              position === 'top' && 'top-[-50px]',
-              position === 'bottom' && 'mt-[20%]',
-              className
-            )}
-          >
-            {text}
-          </div>
-        )}
-      </div>
+    <div className="relative group">
+      {children}
+      {text && (
+        <div
+          className={cn(
+            'absolute bg-gray-700 hidden group-hover:block whitespace-nowrap py-2 px-4 rounded-xl text-white -translate-x-1/2 left-1/2 text-base font-normal z-10',
+            position === 'top' && 'top-[-50px]',
+            position === 'bottom' && 'mt-[20%]',
+            className
+          )}
+        >
+          {text}
+        </div>
+      )}
     </div>
   );
 };
