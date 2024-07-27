c2b68c139f9a3c2afeb79bfd56db3b301833533f
Karim Wael <devkarim@hotmail.com>
2024-07-28 01:59:23 +0300

    feat: add sendFeedback API call

--- src/api/rooms.ts
+++ src/api/rooms.ts
@@ -1,16 +1,16 @@
-import axios from "axios";
+import axios from 'axios';
 
-import { API_HOST } from "@/config/constants";
+import { API_HOST } from '@/config/constants';
 
 const client = axios.create({
   baseURL: API_HOST,
-  responseType: "json",
+  responseType: 'json',
   withCredentials: true,
   validateStatus: (s) => true,
 });
 
 export const createRoom = async (nickname: string) => {
-  const res = await client.post("/word/room/create", {
+  const res = await client.post('/word/room/create', {
     nickname,
   });
   return res.data as CreateRoomResponse;
@@ -33,6 +33,10 @@ export const kickPlayer = async (roomId: string, toKickId: string) => {
     toKickId,
   });
   return res.data;
+};
+
+export const sendFeedback = (email: string, name: string, message: string) => {
+  return client.post('/feedback', { email, name, message, game: 'word' });
 };
 
 // export const changeRoomOptions = async (
