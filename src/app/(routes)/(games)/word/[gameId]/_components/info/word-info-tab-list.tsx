e91ace5e3f314d19da4b47f57c186197b49688ea
Karim Wael <devkarim@hotmail.com>
2024-07-29 19:40:02 +0300

    feat: add sliding animation to tab switching

--- src/app/(routes)/(games)/word/[gameId]/_components/info/word-info-tab-list.tsx
+++ src/app/(routes)/(games)/word/[gameId]/_components/info/word-info-tab-list.tsx
@@ -10,15 +10,21 @@ interface WordInfoTabListProps {}
 const WordInfoTabList: React.FC<WordInfoTabListProps> = ({}) => {
   return (
     <TabList
-      parentClassName="flex flex-col overflow-y-auto flex-grow scrollbar-thin"
+      parentClassName="flex flex-col overflow-y-auto flex-grow scrollbar-thin overflow-x-hidden"
       className="mx-5 mt-10 mb-5 rounded-xl bg-word-side-400/40"
       activeClassName="bg-word-side-200/80 rounded-xl shadow-md"
       tabSwitchClassName="p-3"
     >
-      <TabItem label="الحالية" className="overflow-y-auto scrollbar-none">
+      <TabItem
+        label="الحالية"
+        className="overflow-y-auto scrollbar-none animate-in slide-in-from-left-full duration-300"
+      >
         <WordLeaderboard lastRound />
       </TabItem>
-      <TabItem label="المتصدرون" className="overflow-y-auto scrollbar-none">
+      <TabItem
+        label="المتصدرون"
+        className="overflow-y-auto scrollbar-none animate-in slide-in-from-right-full duration-300"
+      >
         <WordLeaderboard />
       </TabItem>
     </TabList>
