06e85f8b8ce9201edfbdd63fe56127d75f245296
Karim Wael <devkarim@hotmail.com>
2024-07-31 04:34:07 +0300

    feat: add elyra-logo component

---
+++ src/components/ui/elyra-logo.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+
+interface ElyraLogoProps {
+  size?: number;
+  isWhite?: boolean;
+  className?: string;
+  onClick?: () => void;
+}
+
+const ElyraLogo: React.FC<ElyraLogoProps> = ({
+  size = 65,
+  isWhite = true,
+  className,
+  onClick,
+}) => {
+  return (
+    <Image
+      src={isWhite ? '/svg/elyra-white.svg' : '/svg/elyra.svg'}
+      width={size}
+      height={size}
+      alt="elyra-logo"
+      className={className}
+      onClick={onClick}
+    />
+  );
+};
+
+export default ElyraLogo;
