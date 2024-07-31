502fe6db62a5ab1211b557e952077efc40c4d24d
Karim Wael <devkarim@hotmail.com>
2024-07-31 04:38:00 +0300

    feat: redesign home page

--- src/app/(routes)/(games)/word/page.tsx
+++ src/app/(routes)/(games)/word/page.tsx
@@ -1,14 +1,13 @@
-import Link from 'next/link';
 import { Metadata } from 'next';
 
 import { WORD_GAME_NAME } from '@/config/word';
-
 import { APP_NAME_EN } from '@/config/constants';
-import WordLogo from '@/components/word/word-logo';
-import WelcomeUserText from '@/components/user/welcome-user-text';
 
-import WordDescription from './_components/word-description';
-import WordHomeButtons from './_components/word-home-buttons';
+import WordAppbar from './_components/word-appbar';
+import WordPlayHeader from './_components/word-play-header';
+import WordHomeSection from './_components/word-home-section';
+import WordExperience from './_components/word-experience';
+import WordCompetitive from './_components/word-competitive';
 
 export const metadata: Metadata = {
   title: `${APP_NAME_EN} | ${WORD_GAME_NAME}`,
@@ -16,15 +15,12 @@ export const metadata: Metadata = {
 
 export default function WordHomePage() {
   return (
-    <div className="px-4 py-8 h-screen text-white space-y-12 sm:space-y-24">
-      <div className="space-y-6 lg:space-y-8">
-        <Link href="/word">
-          <WordLogo size={100} className="cursor-pointer" />
-        </Link>
-        <WelcomeUserText />
-        <WordDescription />
-      </div>
-      <WordHomeButtons />
+    <div className="min-h-screen">
+      <WordAppbar />
+      <WordHomeSection />
+      <WordPlayHeader />
+      <WordExperience />
+      <WordCompetitive />
     </div>
   );
 }
