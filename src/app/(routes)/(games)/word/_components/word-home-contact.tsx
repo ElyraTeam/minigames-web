ba2b80e2cdbf5780e325b7421561401f207147f0
Karim Wael <devkarim@hotmail.com>
2024-07-31 04:35:40 +0300

    feat: add word-home-contact component

---
+++ src/app/(routes)/(games)/word/_components/word-home-contact.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useState } from 'react';
+import WordButton from '@/components/word/word-button';
+import WordFeedback from '@/components/modals/word-feedback';
+
+interface WordHomeContactProps {}
+
+const WordHomeContact: React.FC<WordHomeContactProps> = ({}) => {
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <>
+      <WordFeedback isOpen={isOpen} onClose={() => setOpen(false)} />
+      <WordButton
+        variant="solid"
+        className="w-28 py-2 bg-transparent hover:bg-white/10 border-word-game-800"
+        onClick={() => setOpen(true)}
+      >
+        تواصل معنا
+      </WordButton>
+    </>
+  );
+};
+
+export default WordHomeContact;
