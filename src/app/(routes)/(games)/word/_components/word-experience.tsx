2deb207c109557da78d706abe07a96ea70893625
Karim Wael <devkarim@hotmail.com>
2024-08-01 03:53:18 +0300

    feat: make word-experience and word-play header responsive

--- src/app/(routes)/(games)/word/_components/word-experience.tsx
+++ src/app/(routes)/(games)/word/_components/word-experience.tsx
@@ -7,21 +7,23 @@ interface WordExperienceProps {}
 const WordExperience: React.FC<WordExperienceProps> = ({}) => {
   return (
     <div className="relative h-[100vh]">
-      <div className="absolute flex justify-between pl-[20%] items-center bottom-0 word-play-background h-full w-full -top-24">
-        <Image
-          src="/img/word-settings.png"
-          alt="word-settings"
-          width={600}
-          height={600}
-          unoptimized={true}
-        />
+      <div className="absolute flex flex-col-reverse lg:flex-row justify-center lg:justify-between lg:pl-[5%] xl:pl-[20%] items-center bottom-0 word-play-background h-full w-full -top-24">
+        <div className="relative w-[300px] h-[320px] xs:w-[350px] xs:h-[370px] lg:w-[500px] lg:h-[500px] xl:w-[600px] xl:h-[600px]">
+          <Image
+            src="/img/word-settings.png"
+            alt="word-settings"
+            className="object-contain"
+            fill
+            unoptimized={true}
+          />
+        </div>
         <WordHomeCard
           title="تحكم فالتجربة"
           description="خصص اللعبة كما تريد عن طريق 
 اختيار الحروف التي تريدها وإضافة أو إزالة فئات حسب رغبتك"
         />
       </div>
-      <div className="absolute -bottom-28 left-[37%] z-50">
+      <div className="absolute -bottom-28 left-[33%] xl:left-[37%] z-50 hidden lg:block">
         <Image
           src="/svg/home-arrow-3.svg"
           alt="home-arrow-3"
--- src/app/(routes)/(games)/word/_components/word-play-header.tsx
+++ src/app/(routes)/(games)/word/_components/word-play-header.tsx
@@ -5,17 +5,19 @@ interface WordPlayHeaderProps {}
 const WordPlayHeader: React.FC<WordPlayHeaderProps> = ({}) => {
   return (
     <div className="relative h-[60vh] flex flex-col items-center justify-center shadow-[rgba(0,0,0,0.8)_0px_5px_45px]">
-      <div className="absolute w-full top-0 h-[260vh] [backdrop-filter:blur(20px)] bg-word-home/50" />
-      <div className="flex flex-col items-center gap-3 z-10">
+      <div className="absolute w-full top-0 h-[360vh] [backdrop-filter:blur(20px)] bg-word-home/50" />
+      <div className="flex flex-col items-center gap-6 z-10">
         <Image
           src="/svg/help-icon.svg"
           width={100}
           height={100}
           alt="help-icon"
         />
-        <p className="text-4xl font-bold">كيفية اللعب</p>
+        <p className="text-2xl xs:text-3xl lg:text-4xl font-bold">
+          كيفية اللعب
+        </p>
       </div>
-      <div className="absolute -bottom-32 left-[35%] z-20">
+      <div className="absolute -bottom-32 left-[27%] xl:left-[35%] z-20 hidden lg:block">
         <Image
           src="/svg/home-arrow-2.svg"
           alt="home-arrow-2"
