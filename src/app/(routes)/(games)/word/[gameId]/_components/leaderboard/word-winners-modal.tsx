7eb37f7f68b5e3022eacaac44d2adafcf71dec4f
Karim Wael <devkarim@hotmail.com>
2024-08-01 04:04:43 +0300

    feat: add word-winners-modal component

---
+++ src/app/(routes)/(games)/word/[gameId]/_components/leaderboard/word-winners-modal.tsx
@@ -0,0 +1,67 @@
+import { useMemo } from 'react';
+import { FaMedal } from 'react-icons/fa';
+
+import { uid } from '@/lib/utils';
+import usePlayersStore from '@/state/players';
+import Modal from '@/components/modals/modal';
+
+import WordWinner from './word-winner';
+
+interface WordWinnersModalProps {
+  isOpen: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+const WordWinnersModal: React.FC<WordWinnersModalProps> = ({
+  isOpen,
+  onOpenChange,
+}) => {
+  const gamePlayers = usePlayersStore((state) => state.players?.players);
+
+  const sortedPlayers = useMemo(() => {
+    return [...(gamePlayers || [])].sort((a, b) => b.totalScore - a.totalScore);
+  }, [gamePlayers]);
+
+  const generatePlayer = () => {
+    const id = uid();
+    return { id, nickname: '-----', totalScore: 0 };
+  };
+
+  const getRankByIndex = (index: number) => {
+    if (index === 1) return 1;
+    if (index === 0) return 3;
+    if (index === 2) return 2;
+    return index;
+  };
+
+  const firstPlr = sortedPlayers[0] ?? generatePlayer();
+  const secondPlr = sortedPlayers[1] ?? generatePlayer();
+  const thirdPlr = sortedPlayers[2] ?? generatePlayer();
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onOpenChange={onOpenChange}
+      className="p-0 overflow-hidden border-none w-[90%] lg:w-[500px]"
+      closeClassName="text-white"
+    >
+      <div className="flex flex-col items-center bg-word-game-800 rounded-b-2xl gap-3 p-8 text-white">
+        <FaMedal className="text-4xl" />
+        <h4 className="font-bold">انتهت اللعبة!</h4>
+      </div>
+      <div className="flex gap-2 px-8 py-4 h-64">
+        {[thirdPlr, firstPlr, secondPlr].map((plr, index) => (
+          <WordWinner
+            key={`word-winner-${plr.id}`}
+            name={plr.nickname}
+            score={plr.totalScore}
+            rank={getRankByIndex(index)}
+            height={(plr.totalScore / firstPlr.totalScore) * 100}
+          />
+        ))}
+      </div>
+    </Modal>
+  );
+};
+
+export default WordWinnersModal;
