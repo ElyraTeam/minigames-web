ac31d62c9725b49ab38d6bc7956daa3b1d38128d
Karim Wael <devkarim@hotmail.com>
2024-08-01 03:52:26 +0300

    feat: setup word-public-rooms-button component
    Add word-public-rooms-button component but still in maintenance.

---
+++ src/app/(routes)/(games)/word/_components/word-public-rooms-button.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useState } from 'react';
+
+import WordButton from '@/components/word/word-button';
+import MaintenanceModal from '@/components/modals/maintenance-modal';
+
+interface WordPublicRoomsButtonProps {}
+
+const WordPublicRoomsButton: React.FC<WordPublicRoomsButtonProps> = ({}) => {
+  const [isMaintenanceOpen, setMaintenanceOpen] = useState(false);
+
+  return (
+    <>
+      <MaintenanceModal
+        isOpen={isMaintenanceOpen}
+        onOpenChange={(open) => setMaintenanceOpen(open)}
+      />
+      <WordButton
+        variant="outline"
+        className="text-lg rounded-2xl hover:bg-white/10"
+        onClick={() => setMaintenanceOpen(true)}
+      >
+        جميع الغرف
+      </WordButton>
+    </>
+  );
+};
+
+export default WordPublicRoomsButton;
