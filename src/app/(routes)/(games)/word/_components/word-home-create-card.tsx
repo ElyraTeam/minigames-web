185a7d8901b417fdc0ea2bd5cbebc362afa1260a
Karim Wael <devkarim@hotmail.com>
2024-08-01 03:54:15 +0300

    feat: make word-home-create-card responsive

--- src/app/(routes)/(games)/word/_components/word-home-create-card.tsx
+++ src/app/(routes)/(games)/word/_components/word-home-create-card.tsx
@@ -1,21 +1,15 @@
-import WordButton from '@/components/word/word-button';
-
 import WordCreateGame from './word-create-game';
+import WordPublicRoomsButton from './word-public-rooms-button';
 
 interface WordHomeCreateCardProps {}
 
 const WordHomeCreateCard: React.FC<WordHomeCreateCardProps> = ({}) => {
   return (
-    <div className="flex flex-col items-center gap-20 justify-center border-2 border-word-game rounded-3xl py-20 px-12 w-80">
+    <div className="flex flex-col items-center gap-16 lg:gap-20 justify-center border-2 border-word-game rounded-3xl py-16 lg:py-20 px-12 w-80">
       <h2 className="font-bold">ابدأ اللعب</h2>
       <div className="space-y-3">
         <WordCreateGame />
-        <WordButton
-          variant="outline"
-          className="text-lg rounded-2xl hover:bg-white/10"
-        >
-          جميع الغرف
-        </WordButton>
+        <WordPublicRoomsButton />
       </div>
     </div>
   );
