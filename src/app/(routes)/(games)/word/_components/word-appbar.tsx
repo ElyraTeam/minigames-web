621d337926c9dcd6a128ca63ffba9c7df7cf9306
Karim Wael <devkarim@hotmail.com>
2024-08-01 03:58:54 +0300

    refactor: make word-appbar component responsive

--- src/app/(routes)/(games)/word/_components/word-appbar.tsx
+++ src/app/(routes)/(games)/word/_components/word-appbar.tsx
@@ -1,13 +1,11 @@
 import Link from 'next/link';
-import { IoPerson } from 'react-icons/io5';
 
 import { WORD_GAME_NAME_AR } from '@/config/word';
 import WordLogo from '@/components/word/word-logo';
 import ElyraLogo from '@/components/ui/elyra-logo';
-import WordButton from '@/components/word/word-button';
 
-import WordNickname from './word-nickname';
-import WordHomeContact from './word-home-contact';
+import WordAppbarMobile from './word-appbar-mobile';
+import WordAppbarButtons from './word-appbar-buttons';
 import WordAppbarBackground from './word-appbar-background';
 
 interface WordAppbarProps {}
@@ -16,28 +14,20 @@ const WordAppbar: React.FC<WordAppbarProps> = ({}) => {
   return (
     <>
       <WordAppbarBackground>
-        <div className="flex items-center gap-5">
+        <div className="flex items-center gap-3 lg:gap-5">
           <ElyraLogo size={60} />
           <div className="border-r-2 border-white/10 h-8 " />
           <div className="flex items-center gap-3">
             <Link href="/word">
               <WordLogo size={65} />
             </Link>
-            <p className="font-bold text-xl">{WORD_GAME_NAME_AR}</p>
+            <p className="font-bold text-lg lg:text-xl">{WORD_GAME_NAME_AR}</p>
           </div>
         </div>
-        <div className="flex  items-center gap-5">
-          <Link href="/getstarted">
-            <WordButton
-              variant="solid"
-              className="w-28 py-2 bg-word-game-800 hover:bg-word-game-800/80 border-none space-x-1 rtl:space-x-reverse flex items-center justify-center shadow-[inset_0_0_10px_rgba(0,0,0,0.5)]"
-            >
-              <IoPerson className="inline text-lg" />
-              <WordNickname />
-            </WordButton>
-          </Link>
-          <WordHomeContact />
+        <div className="w-56 items-center gap-5 hidden sm:flex">
+          <WordAppbarButtons />
         </div>
+        <WordAppbarMobile />
       </WordAppbarBackground>
     </>
   );
