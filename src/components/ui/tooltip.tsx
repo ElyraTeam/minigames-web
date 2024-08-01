7787b41798f62d91afae99ef7ebc61b5128a0f72
Karim Wael <devkarim@hotmail.com>
2024-08-02 00:09:01 +0300

    feat: use hybrid-tooltip for general purpose tooltip

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -2,30 +2,11 @@
 
 import { cn } from '@/lib/utils';
 
-import * as React from 'react';
-import * as TooltipPrimitive from '@radix-ui/react-tooltip';
-
-const TooltipProvider = TooltipPrimitive.Provider;
-
-const TooltipRoot = TooltipPrimitive.Root;
-
-const TooltipTrigger = TooltipPrimitive.Trigger;
-
-const TooltipContent = React.forwardRef<
-  React.ElementRef<typeof TooltipPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
->(({ className, sideOffset = 4, ...props }, ref) => (
-  <TooltipPrimitive.Content
-    ref={ref}
-    sideOffset={sideOffset}
-    className={cn(
-      'z-50 overflow-hidden rounded-md border px-3 py-1.5 text-sm shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 m-1',
-      className
-    )}
-    {...props}
-  />
-));
-TooltipContent.displayName = TooltipPrimitive.Content.displayName;
+import {
+  HybridTooltip,
+  HybridTooltipContent,
+  HybridTooltipTrigger,
+} from './hybrid-tooltip';
 
 interface TooltipProps {
   children?: React.ReactNode;
@@ -43,25 +24,21 @@ const Tooltip: React.FC<TooltipProps> = ({
   delayDuration,
 }) => {
   return (
-    <TooltipProvider>
-      <TooltipRoot delayDuration={delayDuration}>
-        <TooltipTrigger asChild>{children}</TooltipTrigger>
-        {text && (
-          <TooltipContent
-            side={position}
-            className={cn(
-              'bg-gray-700 whitespace-nowrap py-2 px-4 rounded-xl text-white text-sm font-normal border-none',
-              className
-            )}
-          >
-            {text}
-          </TooltipContent>
-        )}
-      </TooltipRoot>
-    </TooltipProvider>
+    <HybridTooltip delayDuration={delayDuration}>
+      <HybridTooltipTrigger asChild>{children}</HybridTooltipTrigger>
+      {text && (
+        <HybridTooltipContent
+          side={position}
+          className={cn(
+            'bg-gray-700 whitespace-nowrap py-2 px-4 rounded-xl text-white text-sm font-normal border-none',
+            className
+          )}
+        >
+          {text}
+        </HybridTooltipContent>
+      )}
+    </HybridTooltip>
   );
 };
 
 export default Tooltip;
-
-export { TooltipRoot, TooltipTrigger, TooltipContent, TooltipProvider };
