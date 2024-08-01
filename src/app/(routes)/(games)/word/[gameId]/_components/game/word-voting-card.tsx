67d331aab5609874b746f34e88ae51f044f98600
Karim Wael <devkarim@hotmail.com>
2024-08-02 00:09:41 +0300

    refactor: make voting cards responsive on all devices
    Make voting card components responsive on all devices including mobile, tablet, and desktop.

--- src/app/(routes)/(games)/word/[gameId]/_components/game/word-voting-card.tsx
+++ src/app/(routes)/(games)/word/[gameId]/_components/game/word-voting-card.tsx
@@ -48,18 +48,30 @@ const WordVotingCard: React.FC<WordVotingCardProps> = ({
   return (
     <div
       className={cn(
-        'grid grid-cols-[min(150px,100%)_2fr_min(200px,100%)] justify-between items-center bg-word-game/50 rounded-2xl text-center w-full overflow-hidden border-2',
+        'grid grid-rows-[60px_100px_100px] sm:grid-cols-[min(150px,100%)_2fr_min(200px,100%)] sm:grid-rows-none lg:grid-rows-[60px_100px_100px] lg:grid-cols-none 2xl:grid-rows-none 2xl:grid-cols-[min(150px,100%)_2fr_min(200px,100%)] items-center bg-word-game/50 rounded-2xl text-center w-full overflow-hidden border-[3px]',
         selectedCard && 'bg-word-secondary/70'
       )}
     >
-      <p className="text-lg lg:text-xl text-white leading-none border-l-2 border-white/20">
+      <p
+        className="text-lg lg:text-xl text-white leading-none p-3 mx-auto sm:mx-0 lg:mx-auto 2xl
+2xl:mx-0 border-b-4 sm:border-l-2 sm:border-b-0 lg:border-b-4 lg:border-l-0 2xl
+2xl:border-b-0 2xl
+2xl:border-l-2 border-white/30"
+      >
         {name}
       </p>
       <h3 className="text-3xl lg:text-4xl font-black leading-none">{value}</h3>
-      <div className="flex flex-col bg-black/5 py-8 px-9 h-full rtl:flex-col-reverse justify-center items-center gap-4 rounded-2xl self-end">
+      <div
+        className="flex flex-row sm:flex-col sm:rtl:flex-col-reverse lg:flex-row lg:rtl:flex-row-reverse xl:flex-col bg-black/5 py-8 px-2 xs:px-4 2xl
+2xl:px-9 h-full 2xl
+2xl:rtl:flex-col-reverse justify-center items-center rounded-2xl self-end gap-1 sm:gap-3 2xl
+2xl:gap-3"
+      >
         {availableVotes.map((voteNumber) => (
           <div
-            className="flex w-full items-center justify-between gap-7"
+            className="flex flex-col sm:flex-row lg:flex-col 2xl
+2xl:flex-row w-full items-center justify-between gap-2 xs:gap-4 2xl
+2xl:gap-7"
             key={`word-voting-card-${name}-${voteNumber}`}
           >
             <Tooltip
@@ -86,7 +98,7 @@ const WordVotingCard: React.FC<WordVotingCardProps> = ({
               dir="ltr"
               onClick={() => onChangeVote(voteNumber)}
               className={cn(
-                'w-20',
+                'w-16 sm:w-32 lg:w-20',
                 !selectedCard &&
                   vote === voteNumber &&
                   'bg-word-secondary hover:bg-word-secondary/80 transition-colors disabled:hover:bg-word-secondary'
