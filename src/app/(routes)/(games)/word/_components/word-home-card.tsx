1f95663b0573990418f81ee8a740f3312ca69d69
Karim Wael <devkarim@hotmail.com>
2024-08-01 03:56:03 +0300

    feat: make word-home-card responsive

--- src/app/(routes)/(games)/word/_components/word-home-card.tsx
+++ src/app/(routes)/(games)/word/_components/word-home-card.tsx
@@ -5,9 +5,11 @@ interface WordHomeCardProps {
 
 const WordHomeCard: React.FC<WordHomeCardProps> = ({ title, description }) => {
   return (
-    <div className="z-50 max-w-80 space-y-6">
-      <h5 className="text-3xl font-bold text-center">{title}</h5>
-      <div className="text-lg border-2 px-6 py-8 border-word-game rounded-3xl bg-word-home">
+    <div className="z-50 max-w-80 space-y-4 lg:space-y-6">
+      <h5 className="text-xl sm:text-2xl lg:text-3xl font-bold text-center">
+        {title}
+      </h5>
+      <div className="text-sm lg:text-lg border-2 px-4 py-6 lg:px-6 lg:py-8 border-word-game rounded-3xl bg-word-home">
         <p>{description}</p>
       </div>
     </div>
