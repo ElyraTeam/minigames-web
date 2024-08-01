1ca3588894a48c93db659827d431c57b514510d0
Karim Wael <devkarim@hotmail.com>
2024-08-02 01:25:05 +0300

    fix: get nickname from client cookies
    Get nickname from client cookies to fix not receiving proper nickname from nextjs server cookies.

--- src/components/providers/nickname-provider.tsx
+++ src/components/providers/nickname-provider.tsx
@@ -1,27 +1,27 @@
 'use client';
 
 import { useEffect } from 'react';
+import { useCookies } from 'next-client-cookies';
 import { usePathname, useRouter } from 'next/navigation';
 
 import useLocalStore from '@/state/local';
+import { NEXTJS_SESSION_KEY } from '@/config/constants';
 
 interface NicknameProviderProps {
-  nickname?: string;
   children: React.ReactNode;
 }
 
-const NicknameProvider: React.FC<NicknameProviderProps> = ({
-  nickname,
-  children,
-}) => {
+const NicknameProvider: React.FC<NicknameProviderProps> = ({ children }) => {
   const router = useRouter();
+  const cookies = useCookies();
   const pathname = usePathname();
   const setNickname = useLocalStore((state) => state.setNickname);
 
   useEffect(() => {
+    if (!cookies) return;
+    const nickname = cookies.get(NEXTJS_SESSION_KEY);
     if (!nickname) return router.push(`/getstarted?next=${pathname}`);
-    setNickname(nickname);
-  }, [nickname, pathname, router, setNickname]);
+  }, [cookies, pathname, router, setNickname]);
 
   return <>{children}</>;
 };
