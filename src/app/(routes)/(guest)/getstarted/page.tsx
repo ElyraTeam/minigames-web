e63771f0b9347557b5f54c841c56aa452d82c0b0
Karim Wael <devkarim@hotmail.com>
2024-08-01 03:51:52 +0300

    refactor: redesign getstarted page

--- src/app/(routes)/(guest)/getstarted/page.tsx
+++ src/app/(routes)/(guest)/getstarted/page.tsx
@@ -3,9 +3,11 @@ import { Metadata } from 'next';
 import { Suspense } from 'react';
 
 import { APP_NAME_EN } from '@/config/constants';
+import ElyraLogo from '@/components/ui/elyra-logo';
 import WordLogo from '@/components/word/word-logo';
 import AuthorLinks from '@/components/about/author-links';
 import AuthorModal from '@/components/modals/author-modal';
+import WordBackground from '@/components/word/word-background';
 
 import NameInput from './_components/name-input';
 
@@ -15,20 +17,27 @@ export const metadata: Metadata = {
 
 export default function GetStarted() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen gap-24">
-      <div className="flex flex-col items-center gap-6">
-        <Link href="/word">
-          <WordLogo size={100} className="cursor-pointer" />
-        </Link>
-        <h2 className="text-2xl font-semibold">مرحبا بك في كلمة!</h2>
+    <WordBackground className="word-dark-background flex flex-col items-center justify-center h-screen gap-24">
+      <div className="bg-white rounded-3xl pt-8 pb-6">
+        <div className="flex flex-col items-center gap-8 px-12 sm:px-16 pb-7">
+          <Link href="/word">
+            <WordLogo size={100} className="cursor-pointer" />
+          </Link>
+          <h2 className="text-2xl font-semibold">مرحبًا بك في كلمة!</h2>
+          <Suspense>
+            <NameInput />
+          </Suspense>
+          <div className="flex flex-col items-center gap-1">
+            <p className="text-sm font-semibold">تم التطوير بواسطة</p>
+            <AuthorModal>
+              <ElyraLogo size={50} isWhite={false} />
+            </AuthorModal>
+          </div>
+        </div>
+        <div className="border-t-2 border-t-black/10">
+          <AuthorLinks />
+        </div>
       </div>
-      <Suspense>
-        <NameInput />
-      </Suspense>
-      <div className="flex flex-col items-center">
-        <AuthorModal className="text-lg">تطوير</AuthorModal>
-        <AuthorLinks />
-      </div>
-    </main>
+    </WordBackground>
   );
 }
