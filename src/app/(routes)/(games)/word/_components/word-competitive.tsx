5bcd4ce723504137be6951099d0f6829b51f4851
Karim Wael <devkarim@hotmail.com>
2024-08-01 03:53:47 +0300

    feat: add last arrow to word-competitve & make component responsive

--- src/app/(routes)/(games)/word/_components/word-competitive.tsx
+++ src/app/(routes)/(games)/word/_components/word-competitive.tsx
@@ -8,19 +8,29 @@ const WordCompetitive: React.FC<WordCompetitiveProps> = ({}) => {
   return (
     <div className="relative h-[100vh]">
       <div className="absolute items-center bottom-0 word-play-background word-play-revert h-full w-full -top-24">
-        <div className="flex items-center w-full h-full scale-x-[-1] justify-between pr-[20%]">
+        <div className="flex flex-col lg:flex-row items-center w-full h-full scale-x-[-1] justify-center lg:justify-between lg:pr-[5%] xl:pr-[20%]">
           <WordHomeCard
             title="كن اسرع"
             description="تسابق مع باقي اللاعبين واملأ جميع الفئات تنتهي مرحلة الكتابة مع انتهاء اول لاعب"
           />
-          <Image
-            src="/img/word-game.png"
-            alt="word-settings"
-            width={600}
-            height={600}
-            unoptimized={true}
-          />
+          <div className="relative w-[350px] h-[280px] xs:w-[350px] xs:h-[330px] lg:w-[600px] lg:h-[600px]">
+            <Image
+              src="/img/word-game.png"
+              alt="word-settings"
+              className="object-contain"
+              fill
+              unoptimized={true}
+            />
+          </div>
         </div>
+      </div>
+      <div className="absolute -bottom-24 left-[33%] xl:left-[37%] z-50 hidden lg:block">
+        <Image
+          src="/svg/home-arrow-4.svg"
+          alt="home-arrow-4"
+          width={350}
+          height={350}
+        />
       </div>
     </div>
   );
