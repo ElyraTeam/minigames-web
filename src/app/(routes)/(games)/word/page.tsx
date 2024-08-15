928ca04c3bd55502c1161c68b361f675c717b73e
Karim Wael <devkarim@hotmail.com>
2024-08-15 21:58:34 +0300

    refactor: remove unnecessary metadata from word home page

--- src/app/(routes)/(games)/word/page.tsx
+++ src/app/(routes)/(games)/word/page.tsx
@@ -1,18 +1,9 @@
-import { Metadata } from 'next';
-
-import { WORD_GAME_NAME } from '@/config/word';
-import { APP_NAME_EN } from '@/config/constants';
-
 import WordAppbar from './_components/word-appbar';
-import WordPlayHeader from './_components/word-play-header';
-import WordHomeSection from './_components/word-home-section';
-import WordExperience from './_components/word-experience';
-import WordCompetitive from './_components/word-competitive';
 import WordGetPoints from './_components/word-get-points';
-
-export const metadata: Metadata = {
-  title: `${APP_NAME_EN} | ${WORD_GAME_NAME}`,
-};
+import WordExperience from './_components/word-experience';
+import WordPlayHeader from './_components/word-play-header';
+import WordCompetitive from './_components/word-competitive';
+import WordHomeSection from './_components/word-home-section';
 
 export default function WordHomePage() {
   return (
