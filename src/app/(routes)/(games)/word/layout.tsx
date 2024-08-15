9fa27fb881f9ab5ca94bbf5459871566c7c413e3
Karim Wael <devkarim@hotmail.com>
2024-08-15 21:58:11 +0300

    refactor: update WordLayout to include viewport and metadata

--- src/app/(routes)/(games)/word/layout.tsx
+++ src/app/(routes)/(games)/word/layout.tsx
@@ -1,4 +1,28 @@
+import { Metadata, Viewport } from 'next';
+
+import { WORD_GAME_NAME } from '@/config/word';
 import WordBackground from '@/components/word/word-background';
+
+export const viewport: Viewport = {
+  themeColor: '#0DF822',
+};
+
+export const metadata: Metadata = {
+  title: WORD_GAME_NAME,
+  openGraph: {
+    title: WORD_GAME_NAME,
+    description: 'Play Word Game and improve your vocabulary!',
+    url: `/word`,
+    images: [
+      {
+        url: `/logo.png`,
+        width: 512,
+        height: 512,
+        alt: 'Word Logo',
+      },
+    ],
+  },
+};
 
 export default async function WordLayout({
   children,
