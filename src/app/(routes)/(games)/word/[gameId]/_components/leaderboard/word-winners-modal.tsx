73ea484a017fb6dfd4deed993188a24608cddf41
Karim Wael <devkarim@hotmail.com>
2024-08-15 21:25:00 +0300

    refactor: update WordWinnersModal to use winners prop instead of gamePlayers and sortedPlayers

--- src/app/(routes)/(games)/word/[gameId]/_components/leaderboard/word-winners-modal.tsx
+++ src/app/(routes)/(games)/word/[gameId]/_components/leaderboard/word-winners-modal.tsx
@@ -1,33 +1,20 @@
 import { FaMedal } from 'react-icons/fa';
-import { useEffect, useMemo } from 'react';
 
-import { uid } from '@/lib/utils';
-import { WordSound } from '@/config/word';
-import useLocalStore from '@/state/local';
-import usePlayersStore from '@/state/players';
 import Modal from '@/components/modals/modal';
-import useWordSound from '@/hooks/use-word-sound';
 
 import WordWinner from './word-winner';
 
 interface WordWinnersModalProps {
   isOpen: boolean;
   onOpenChange: (open: boolean) => void;
+  winners: Player[];
 }
 
 const WordWinnersModal: React.FC<WordWinnersModalProps> = ({
   isOpen,
   onOpenChange,
+  winners,
 }) => {
-  const [playWinnerSound] = useWordSound(WordSound.AFTER_WIN);
-  const [playLoseSound] = useWordSound(WordSound.AFTER_LOSE);
-  const currentPlayerId = useLocalStore((state) => state.playerId);
-  const gamePlayers = usePlayersStore((state) => state.players?.players);
-
-  const sortedPlayers = useMemo(() => {
-    return [...(gamePlayers || [])].sort((a, b) => b.totalScore - a.totalScore);
-  }, [gamePlayers]);
-
   const getRankByIndex = (index: number) => {
     if (index === 1) return 1;
     if (index === 0) return 3;
@@ -38,30 +25,18 @@ const WordWinnersModal: React.FC<WordWinnersModalProps> = ({
   const renderWinner = (index: number, plr?: Player | null) => {
     return (
       <WordWinner
-        key={`word-winner-${uid()}`}
+        key={`word-winner-${index}`}
         name={plr?.nickname ?? '-----'}
         score={plr?.totalScore ?? 0}
         rank={getRankByIndex(index)}
-        height={((plr?.totalScore ?? 0) / firstPlr?.totalScore) * 100}
+        height={((plr?.totalScore ?? 0) / firstPlr.totalScore) * 100}
       />
     );
   };
 
-  const firstPlr = sortedPlayers[0];
-  const secondPlr = sortedPlayers[1];
-  const thirdPlr = sortedPlayers[2];
-
-  useEffect(() => {
-    if (!isOpen) return;
-    if (currentPlayerId && sortedPlayers[0]) {
-      if (currentPlayerId === sortedPlayers[0].id) {
-        playWinnerSound();
-      } else {
-        playLoseSound();
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentPlayerId, sortedPlayers, isOpen]);
+  const firstPlr = winners[0];
+  const secondPlr = winners[1];
+  const thirdPlr = winners[2];
 
   return (
     <Modal
