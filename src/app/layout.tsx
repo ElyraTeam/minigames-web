888cc32183bfdd03f93c1fb2e7ebe55731a76360
Karim Wael <devkarim@hotmail.com>
2024-08-15 21:57:23 +0300

    refactor: update layout.tsx to use new metadata structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css';
 
 import { TooltipProvider } from '@/components/ui/tooltip-desktop';
-import { APP_NAME_AR, APP_NAME_EN, TEAM_NAME_EN } from '@/config/constants';
+import { APP_NAME_EN, HOST, TEAM_NAME_EN } from '@/config/constants';
 
 import type { Metadata } from 'next';
 import { Cairo } from 'next/font/google';
@@ -10,8 +10,12 @@ import { Toaster } from 'react-hot-toast';
 const font = Cairo({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: `${APP_NAME_EN} | ${APP_NAME_AR}`,
+  title: {
+    default: APP_NAME_EN,
+    template: `${APP_NAME_EN} | %s`,
+  },
   description: `Minigames Website by ${TEAM_NAME_EN}`,
+  metadataBase: new URL(HOST),
 };
 
 export default function RootLayout({
