1d89feaa3c423d97eee8bce118007140a3e2998c
Karim Wael <devkarim@hotmail.com>
2024-08-15 20:04:19 +0300

    refactor: Add winners state to game store

--- src/state/game.ts
+++ src/state/game.ts
@@ -3,13 +3,17 @@ import { devtools } from 'zustand/middleware';
 
 interface GameState {
   game?: GameSync | null;
+  winners?: Player[] | null;
   setGame: (game: GameSync) => void;
+  setWinners: (winners: Player[]) => void;
 }
 
 const useGameStore = create<GameState>()(
   devtools((set) => ({
     game: null,
+    winners: null,
     setGame: (game) => set(() => ({ game })),
+    setWinners: (winners) => set(() => ({ winners })),
   }))
 );
 
