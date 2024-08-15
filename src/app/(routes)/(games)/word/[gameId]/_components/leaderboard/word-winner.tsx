00cb5e8c4cdd6abdbb2ccf1da460387d155f6519
Karim Wael <devkarim@hotmail.com>
2024-08-15 21:24:44 +0300

    refactor: remove unnecessary code and optimize height animation in word-winner component

--- src/app/(routes)/(games)/word/[gameId]/_components/leaderboard/word-winner.tsx
+++ src/app/(routes)/(games)/word/[gameId]/_components/leaderboard/word-winner.tsx
@@ -1,6 +1,3 @@
-import { useEffect, useState } from 'react';
-import { animated, useSpring } from '@react-spring/web';
-
 import { cn } from '@/lib/utils';
 
 import WordRank from '../word-rank';
@@ -18,47 +15,30 @@ const WordWinner: React.FC<WordWinnerProps> = ({
   rank,
   height,
 }) => {
-  const [show, setShow] = useState(false);
-
-  const props = useSpring({
-    from: { height: `0%` },
-    to: { height: `${height}%` },
-    config: {
-      mass: 10,
-      friction: 70,
-      tension: 100,
-    },
-  });
-
-  // Fix refreshing not animating the leaderboard
-  useEffect(() => {
-    setTimeout(() => {
-      setShow(true);
-    }, 100);
-  }, []);
-
-  if (!show) return null;
-
   return (
-    <div className="flex flex-col items-center justify-end w-1/3 gap-3">
-      <div className="flex gap-2">
-        <WordRank rank={rank} />
-        <p className="text-xl whitespace-nowrap">{name}</p>
-      </div>
-      <animated.div
-        className={cn(
-          'w-full h-52 bg-yellow-500 flex flex-col items-center pt-4 transition-all min-h-14',
-          rank === 1 &&
-            'bg-gradient-to-l from-yellow-600 to-white to-[300%] text-white',
-          rank === 2 &&
-            'bg-gradient-to-l from-gray-500 to-white to-[300%] text-white rounded-bl-2xl',
-          rank === 3 &&
-            'bg-gradient-to-l from-yellow-800 to-white to-[300%] text-white rounded-br-2xl'
-        )}
-        style={props}
+    <div className="flex flex-col justify-end w-1/3">
+      <div
+        className="flex flex-col gap-3 justify-end items-center w-full h-52 min-h-14"
+        style={{ height: `${height}%` }}
       >
-        <h3 className="font-semibold">{score}</h3>
-      </animated.div>
+        <div className="flex gap-2">
+          <WordRank rank={rank} />
+          <p className="text-xl whitespace-nowrap">{name}</p>
+        </div>
+        <div
+          className={cn(
+            'w-full h-full bg-yellow-500 flex flex-col items-center transition-all animate-height py-3',
+            rank === 1 &&
+              'bg-gradient-to-l from-yellow-600 to-white to-[300%] text-white',
+            rank === 2 &&
+              'bg-gradient-to-l from-gray-500 to-white to-[300%] text-white rounded-bl-2xl',
+            rank === 3 &&
+              'bg-gradient-to-l from-yellow-800 to-white to-[300%] text-white rounded-br-2xl'
+          )}
+        >
+          <h3 className="font-semibold">{score}</h3>
+        </div>
+      </div>
     </div>
   );
 };
