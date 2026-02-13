3b958cdf26241de94ee545cc29cec8e6c9dc5014
devkarim <devkarim@hotmail.com>
2026-02-13 16:09:37 +0200

    feat: add mobile chat state management
    Add isMobileChatOpen state and setMobileChatOpen action to chat store

--- src/state/chat.ts
+++ src/state/chat.ts
@@ -6,16 +6,19 @@ import { uid } from '@/lib/utils';
 interface ChatState {
   messages: ChatMessage[];
   newMessages: number;
+  isMobileChatOpen: boolean;
   addChatMessage: (msg: ChatMessageServer) => void;
   resetChatMessages: () => void;
   addNewMessage: () => void;
   clearNewMessages: () => void;
+  setMobileChatOpen: (open: boolean) => void;
 }
 
 const useChatStore = create<ChatState>()(
   devtools((set) => ({
     messages: [],
     newMessages: 0,
+    isMobileChatOpen: false,
     addChatMessage: (msg) =>
       set(({ messages }) => ({
         messages: [
@@ -27,6 +30,7 @@ const useChatStore = create<ChatState>()(
     addNewMessage: () =>
       set((state) => ({ newMessages: state.newMessages + 1 })),
     clearNewMessages: () => set(() => ({ newMessages: 0 })),
+    setMobileChatOpen: (open) => set(() => ({ isMobileChatOpen: open })),
   }))
 );
 
